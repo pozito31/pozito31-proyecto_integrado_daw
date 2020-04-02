7821fe20c4702ecab3530a68218455ffb7a45c0e
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 15:20:49 +0200

    terminados

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
-export class AppComponent {
-  title = 'GitHub Browser';
+export class AppComponent implements OnInit {
+  constructor (private GitSearchService : GitSearchService) {
+
+  }
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then( (response) => {
+      alert("Total Libraries Found:" + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText);
+    })
+  }
+  title = 'app is functional!';
 }
