467b67d31ca629bb3178a327651c536c75cda157
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 15:14:38 +0200

    consumir servicio

--- angular-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { GitSearchService } from './git-search.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GitSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
