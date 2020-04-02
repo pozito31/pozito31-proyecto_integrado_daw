0e051f8d5256f18ce3cc2f69e207fb85a0de72b7
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 16:24:26 +0200

    agregado todo

--- angular-fundamentals/src/app/git-search/git-search.component.ts
+++ angular-fundamentals/src/app/git-search/git-search.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GitSearchService } from '../git-search.service'
+import { GitSearch } from '../git-search'
 @Component({
   selector: 'app-git-search',
   templateUrl: './git-search.component.html',
   styleUrls: ['./git-search.component.css']
 })
 export class GitSearchComponent implements OnInit {
+  searchResults: GitSearch;
+  constructor(private GitSearchService: GitSearchService) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then( (response) => {
+      this.searchResults = response;
+    }, (error) => {
+      alert("Error: " + error.statusText)
+    })
+  }
 
-  ngOnInit(): void {
+  gitSearch = (query) => {
+    this.GitSearchService.gitSearch(query).then( (response) => {
+      this.searchResults = response;
+    }, (error) => {
+      alert("Error: " + error.statusText)
+    })
   }
 
 }
