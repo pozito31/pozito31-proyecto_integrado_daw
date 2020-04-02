f13c2d3b9260e0ad6d124120fa0ac2606d4f3e28
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 15:00:25 +0200

    crear metodo servicio

--- angular-fundamentals/src/app/git-search.service.ts
+++ angular-fundamentals/src/app/git-search.service.ts
@@ -4,6 +4,20 @@ import { GitSearch } from './git-search'
   providedIn: 'root'
 })
 export class GitSearchService {
-
+  cachedValues: Array<{
+    [query: string]: GitSearch
+  }> = [];
   constructor() { }
+
+  gitSearch = (query: string) => {
+    let promise = new Promise((resolve, reject) => {
+        if (this.cachedValues[query]) {
+            resolve(this.cachedValues[query])
+        }
+        else {
+            resolve("Placeholder");
+        }
+    })
+    return promise;
+  }
 }
