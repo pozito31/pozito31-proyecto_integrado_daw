4be870460b3918d38b408638ca8b7881676fcac7
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 15:11:55 +0200

    terminado

--- angular-fundamentals/src/app/git-search.service.ts
+++ angular-fundamentals/src/app/git-search.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search'
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,15 +9,23 @@ export class GitSearchService {
   cachedValues: Array<{
     [query: string]: GitSearch
   }> = [];
-  constructor() { }
+  constructor(private http: HttpClient) {
+      
+   }
 
-  gitSearch = (query: string) => {
-    let promise = new Promise((resolve, reject) => {
+   gitSearch = (query: string): Promise<GitSearch> => {
+    let promise = new Promise<GitSearch>((resolve, reject) => {
         if (this.cachedValues[query]) {
             resolve(this.cachedValues[query])
         }
         else {
-            resolve("Placeholder");
+            this.http.get('https://api.github.com/search/repositories?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitSearch)
+            }, (error) => {
+                reject(error);
+            })
         }
     })
     return promise;
