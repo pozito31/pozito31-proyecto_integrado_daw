60cca58a4e4abb89a9d382a13823c42eca99f876
pozito31 <jessicapozo198715@gmail.com>
2020-04-02 17:37:50 +0200

    importando formsModule

--- angular-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,6 +15,8 @@ import { GitSearchComponent } from './git-search/git-search.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
