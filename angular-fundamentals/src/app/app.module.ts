00850c34b6239960803e8fb4af23a9fa7ab79eb3
pozito31 <jessicapozo198715@gmail.com>
2020-04-03 18:37:09 +0200

    nose

--- angular-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+
 const appRoutes: Routes = [
   { path: '', 
     component: HomePageComponent 
