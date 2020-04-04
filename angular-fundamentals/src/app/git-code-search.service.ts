57af3b88bccffab5d073bf66cf48c0837a7ed2a7
pozito31 <jessicapozo198715@gmail.com>
2020-04-04 18:35:46 +0200

    importar HttpClient y Observable

--- angular-fundamentals/src/app/git-code-search.service.ts
+++ angular-fundamentals/src/app/git-code-search.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GitCodeSearch } from './git-code-search'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
