20d5b12fd0f544250557e56dd4763e80e472497f
pozito31 <jessicapozo198715@gmail.com>
2020-04-04 14:26:30 +0200

    agregando y modificando cosas

--- angular-fundamentals/src/app/git-search.service.ts
+++ angular-fundamentals/src/app/git-search.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search'
-import { GitUsers } from './git-users';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
-  cachedValues: Array<{
+  cachedSearches: Array<{
     [query: string]: GitSearch
   }> = [];
+  search: Observable<GitSearch>;
   constructor(private http: HttpClient) {
       
    }
@@ -32,21 +32,4 @@ export class GitSearchService {
     return promise;
   }
 
-  gitUsers = (query: string): Promise<GitUsers> => {
-    let promise = new Promise<GitUsers>((resolve, reject) => {
-        if (this.cachedValues[query]) {
-            resolve(this.cachedValues[query])
-        }
-        else {
-            this.http.get('https://api.github.com/search/users?q=' + query)
-            .toPromise()
-            .then( (response) => {
-                resolve(response as GitUsers)
-            }, (error) => {
-                reject(error);
-            })
-        }
-    })
-    return promise;
-  }
 }
--- angular-fundamentals/src/app/git-users.service.ts
+++ angular-fundamentals/src/app/git-users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { GitUsers } from './git-users';
-import { GitSearch } from './git-search'
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 
@@ -26,24 +25,6 @@ export class GitUsersService {
             .toPromise()
             .then( (response) => {
                 resolve(response as GitUsers)
-            }, (error) => {
-                reject(error);
-            })
-        }
-    })
-    return promise;
-  }
-
-  gitSearch = (query: string): Promise<GitSearch> => {
-    let promise = new Promise<GitSearch>((resolve, reject) => {
-        if (this.cachedValues[query]) {
-            resolve(this.cachedValues[query])
-        }
-        else {
-            this.http.get('https://api.github.com/search/repositories?q=' + query)
-            .toPromise()
-            .then( (response) => {
-                resolve(response as GitSearch)
             }, (error) => {
                 reject(error);
             })
