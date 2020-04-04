5566f4e9c015d8ad883bb1e433120e7f87337243
pozito31 <jessicapozo198715@gmail.com>
2020-04-04 14:04:59 +0200

    agregando cosas

--- angular-fundamentals/src/app/git-search.service.ts
+++ angular-fundamentals/src/app/git-search.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search'
+import { GitUsers } from './git-users';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 @Injectable({
@@ -23,6 +24,24 @@ export class GitSearchService {
             .toPromise()
             .then( (response) => {
                 resolve(response as GitSearch)
+            }, (error) => {
+                reject(error);
+            })
+        }
+    })
+    return promise;
+  }
+
+  gitUsers = (query: string): Promise<GitUsers> => {
+    let promise = new Promise<GitUsers>((resolve, reject) => {
+        if (this.cachedValues[query]) {
+            resolve(this.cachedValues[query])
+        }
+        else {
+            this.http.get('https://api.github.com/search/users?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitUsers)
             }, (error) => {
                 reject(error);
             })
