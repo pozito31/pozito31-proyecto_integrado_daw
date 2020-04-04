76ac52ff985efc02310e2ad23b510c0b371379b8
pozito31 <jessicapozo198715@gmail.com>
2020-04-04 16:09:44 +0200

    modificado

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GitSearchService } from './git-search.service';
-import { GitUsersService } from './git-users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,16 +7,11 @@ import { GitUsersService } from './git-users.service';
   providers: [GitSearchService]
 })
 export class AppComponent implements OnInit {
-  constructor(private GitSearchService: GitSearchService, private GitUsersService: GitUsersService) {
+  constructor(private GitSearchService: GitSearchService) {
 
   }
   ngOnInit() {
     this.GitSearchService.gitSearch('angular').then( (response) => {
-      alert("Total Libraries Found:" + response.total_count);
-    }, (error) => {
-      alert("Error: " + error.statusText);
-    })
-    this.GitUsersService.gitUsers('angular').then( (response) => {
       alert("Total Libraries Found:" + response.total_count);
     }, (error) => {
       alert("Error: " + error.statusText);
