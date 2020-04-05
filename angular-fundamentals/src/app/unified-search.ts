da60eb684c4851f394feb600980eb7eefaa2eb61
pozito31 <jessicapozo198715@gmail.com>
2020-04-05 16:06:18 +0200

    terfinado unified-search.ts

--- angular-fundamentals/src/app/unified-search.ts
+++ angular-fundamentals/src/app/unified-search.ts
@@ -2,4 +2,6 @@ import { GitSearch } from './git-search';
 import { GitCodeSearch } from './git-code-search';
 
 export interface UnifiedSearch {
+    repositories: GitSearch,
+    code: GitCodeSearch
 }
