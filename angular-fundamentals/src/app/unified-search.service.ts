c29197709631a6a0bffc20b297acbc8f45c775cf
pozito31 <jessicapozo198715@gmail.com>
2020-04-05 16:18:24 +0200

    importando cosas al fichero de unified search

--- angular-fundamentals/src/app/unified-search.service.ts
+++ angular-fundamentals/src/app/unified-search.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { UnifiedSearch } from './unified-search';
+import { Observable } from 'rxjs/Observable';
+import { GitSearchService } from './git-search.service'
+import { GitCodeSearchService } from './git-code-search.service'
 
 @Injectable({
   providedIn: 'root'
