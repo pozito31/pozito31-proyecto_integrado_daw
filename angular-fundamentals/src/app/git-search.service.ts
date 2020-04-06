c9eccc0919914df42c79c275a2473883be1ee13f
pozito31 <jessicapozo198715@gmail.com>
2020-04-06 16:18:09 +0200

    modificado

--- angular-fundamentals/src/app/git-search.service.ts
+++ angular-fundamentals/src/app/git-search.service.ts
@@ -1,40 +1,35 @@
-import { Injectable } from '@angular/core';
-import { GitSearch } from './git-search'
+import { Injectable, Inject } from '@angular/core';
+import { GitSearch } from './git-search';
 import { GitUsers } from './git-users';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { exhaustMap, scan, mapTo, map, publishReplay, startWith, refCount, first, filter, switchMap } from 'rxjs/operators';
-@Injectable({
-  providedIn: 'root'
-})
+import 'rxjs/add/operator/publishReplay';
+
+
+@Injectable()
 export class GitSearchService {
-  private cadenahttp: string;
-  cachedValue: string;
-    search: Observable<GitSearch>;
+    cachedValue: string;    
   cachedUsers: Array<{
       [query: string]: GitUsers
   }> = [];
+  search: Observable<GitSearch>;
   constructor(private http: HttpClient) {
-      
-   }
-
-   gitSearch: Function = (query: string, page: number): Observable<GitSearch> => {
-
-    if (page === null) {
-      this.cadenahttp = query;
-    }
-    else {
-      this.cadenahttp = query + '&page=' + page;
-    }
-
-      this.search = this.http.get<GitSearch>('https://api.github.com/search/repositories?q=' + this.cadenahttp).pipe(        
-        publishReplay(1),
-        refCount());
-      this.cachedValue = query;      
-    
-    return this.search;
   }
 
+  gitSearch : Function = (query: string) : Observable<GitSearch> => {
+    if (!this.search) {
+        this.search = this.http.get<GitSearch>('https://api.github.com/search/repositories?q=' + query)
+        .publishReplay(1)
+        .refCount();
+        this.cachedValue = query;
+    }
+    else if (this.cachedValue !== query) {
+        this.search = null;
+        this.gitSearch(query);
+    }
+    return this.search;
+  }
   gitUsers = (query: string) : Promise<GitUsers> => {
     let promise = new Promise<GitUsers>((resolve, reject) => {
         if (this.cachedUsers[query]) {
