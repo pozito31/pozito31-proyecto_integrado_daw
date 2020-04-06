c0103d4ec53ef2e698a24d0ccfe6c9417939b7eb
pozito31 <jessicapozo198715@gmail.com>
2020-04-06 16:11:58 +0200

    modificado

--- angular-fundamentals/src/app/git-code-search.service.ts
+++ angular-fundamentals/src/app/git-code-search.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
-import { GitCodeSearch } from './git-code-search'
+import { GitCodeSearch } from './git-code-search';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { exhaustMap, scan, mapTo, map, publishReplay, startWith, refCount, first, filter, switchMap } from 'rxjs/operators';
+import 'rxjs/add/operator/publishReplay';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GitCodeSearchService {
   cachedValue: string;    
   search: Observable<GitCodeSearch>;
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {
+  }    
   codeSearch : Function = (query: string) : Observable<GitCodeSearch> => {
     if (query.indexOf('user') <= -1) {
       query = query + '+user:angular';
     }
     if (!this.search) {
-        this.search = this.http.get<GitCodeSearch>('https://api.github.com/search/code?q=' + query).pipe(
-          publishReplay(1),
-          refCount());
+        this.search = this.http.get<GitCodeSearch>('https://api.github.com/search/code?q=' + query)
+        .publishReplay(1)
+        .refCount();
         this.cachedValue = query;
     }
     else if (this.cachedValue !== query) {
