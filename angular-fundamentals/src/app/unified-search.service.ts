94872109c32769a3bb90a4155aff3d119afa6e16
pozito31 <jessicapozo198715@gmail.com>
2020-04-06 11:59:52 +0200

    modificando los import de unified

--- angular-fundamentals/src/app/unified-search.service.ts
+++ angular-fundamentals/src/app/unified-search.service.ts
@@ -5,20 +5,12 @@ import { GitSearchService } from './git-search.service';
 import { GitCodeSearchService } from './git-code-search.service';
 import { GitSearch } from './git-search';
 import { GitCodeSearch } from './git-code-search';
-import {forkJoin, of } from 'rxjs';
-import 'rxjs/add/operator/map';
+import { forkJoin, of } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Injectable()
 export class UnifiedSearchService {
 
   constructor(private searchService : GitSearchService, private codeSearchService : GitCodeSearchService) { }
-  unifiedSearch : Function = (query: string) : Observable<UnifiedSearch> => {
-    return Observable.forkJoin(this.searchService.gitSearch(query), this.codeSearchService.codeSearch(query))
-    .map( (response : [GitSearch, GitCodeSearch]) => {
-      return {
-        'repositories' : response[0],
-        'code': response[1]
-      }
-    })
-  } 
+  
 }
