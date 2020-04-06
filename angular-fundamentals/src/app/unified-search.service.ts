9c27a6bcbf63bfe875c21653f946df68e2fdcf97
pozito31 <jessicapozo198715@gmail.com>
2020-04-06 15:49:15 +0200

    modificado

--- angular-fundamentals/src/app/unified-search.service.ts
+++ angular-fundamentals/src/app/unified-search.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { UnifiedSearch } from './unified-search';
-import { Observable } from 'rxjs/Observable';
-import { GitSearchService } from './git-search.service';
-import { GitCodeSearchService } from './git-code-search.service';
-import { GitSearch } from './git-search';
-import { GitCodeSearch } from './git-code-search';
-import { forkJoin, of, concat } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { GitSearchService } from './git-search.service'
+import { GitCodeSearchService } from './git-code-search.service'
+import { GitSearch } from './git-search'
+import { GitCodeSearch } from './git-code-search'
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UnifiedSearchService {
 
   constructor(private searchService : GitSearchService, private codeSearchService : GitCodeSearchService) { }
-  combineLatest : Function = (query: string) : Observable<UnifiedSearch> => {
-    return concat(this.searchService.gitSearch(query), this.codeSearchService.codeSearch(query)).pipe(
-      map( (response : [GitSearch, GitCodeSearch]) =>{
-        return {
-          'repositories' : response[0],
-          'code': response[1]
-        }
-      })
-    )
-  }
+  unifiedSearch : Function = (query: string) : Observable<UnifiedSearch> => {
+    return forkJoin(this.searchService.gitSearch(query), this.codeSearchService.codeSearch(query))
+    .map( (response : [GitSearch, GitCodeSearch]) => {
+      return {
+        'repositories' : response[0],
+        'code': response[1]
+      }
+    })
+  } 
 }
