7880cfebb036ab761ab8f5693cebc83503e3dd39
pozito31 <jessicapozo198715@gmail.com>
2020-04-06 14:43:18 +0200

    modificado forkJoin por concat

--- angular-fundamentals/src/app/unified-search.service.ts
+++ angular-fundamentals/src/app/unified-search.service.ts
@@ -5,7 +5,7 @@ import { GitSearchService } from './git-search.service';
 import { GitCodeSearchService } from './git-code-search.service';
 import { GitSearch } from './git-search';
 import { GitCodeSearch } from './git-code-search';
-import { forkJoin, of } from 'rxjs';
+import { forkJoin, of, concat } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -13,7 +13,7 @@ export class UnifiedSearchService {
 
   constructor(private searchService : GitSearchService, private codeSearchService : GitCodeSearchService) { }
   unifiedSearch : Function = (query: string) : Observable<UnifiedSearch> => {
-    return forkJoin(this.searchService.gitSearch(query), this.codeSearchService.codeSearch(query)).pipe(
+    return concat(this.searchService.gitSearch(query), this.codeSearchService.codeSearch(query)).pipe(
       map( (response : [GitSearch, GitCodeSearch]) =>{
         return {
           'repositories' : response[0],
