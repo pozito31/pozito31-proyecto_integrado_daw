f798df1d303539a3f2dee3357ae486d24743d2e1
pozito31 <jessicapozo198715@gmail.com>
2020-04-07 12:48:14 +0200

    modificado

--- angular-fundamentals/src/app/no-special-chars.directive.ts
+++ angular-fundamentals/src/app/no-special-chars.directive.ts
@@ -5,16 +5,17 @@ import { NG_VALIDATORS, Validator, FormControl, ValidationErrors } from '@angula
   selector: '[appNoSpecialChars]',
   providers: [{provide: NG_VALIDATORS, useExisting: NoSpecialCharsDirective, multi: true}]
 })
-export class NoSpecialCharsDirective implements Validator{
+export class NoSpecialCharsDirective implements Validator {
 
   constructor() { }
+  
   validate(c: FormControl): ValidationErrors {
-    const hasSpecialChars = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(c.value);
-    const message = {
-      'hasSpecialChars': {
-        'message': 'No Special Characters Allowed'
-      }
-    };
-    return hasSpecialChars ? message : null;
-  }
+   const hasSpecialChars = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/.test(c.value);
+   const message = {
+     'hasSpecialChars': { 
+       'message': 'No Special Characters Allowed'
+     }
+   };
+   return hasSpecialChars ? message : null;
+ }
 }
