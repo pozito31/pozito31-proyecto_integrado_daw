dfbb28bf8c1a6aef03052f15c2372fdbd46b3b6b
pozito31 <jessicapozo198715@gmail.com>
2020-04-08 18:14:19 +0200

    contenido de las tablas de fabricante puesto

--- carpeta_proyecto/database/migrations/2020_04_08_160243_fabricantes_migration.php
+++ carpeta_proyecto/database/migrations/2020_04_08_160243_fabricantes_migration.php
@@ -14,7 +14,12 @@ class FabricantesMigration extends Migration
     public function up()
     {
         Schema::create('fabricantes', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('telefono');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
