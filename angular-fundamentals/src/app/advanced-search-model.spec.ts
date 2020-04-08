6c8ffa3185126a27ec54398e02be6ef964d9e739
pozito31 <jessicapozo198715@gmail.com>
2020-04-08 13:39:06 +0200

    eliminar aplicacion

--- angular-fundamentals/src/app/advanced-search-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AdvancedSearchModel } from './advanced-search-model';
-
-describe('AdvancedSearchModel', () => {
-  it('should create an instance', () => {
-    expect(new AdvancedSearchModel()).toBeTruthy();
-  });
-});
--- angular-fundamentals/src/app/no-special-chars.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoSpecialCharsDirective } from './no-special-chars.directive';
-
-describe('NoSpecialCharsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NoSpecialCharsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
