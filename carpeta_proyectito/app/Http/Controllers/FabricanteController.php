f3fd94d49302e03b92600a996281d6358028a6cd
pozito31 <jessicapozo198715@gmail.com>
2020-04-09 17:09:43 +0200

    modificando fabricanteController

--- carpeta_proyectito/app/Http/Controllers/FabricanteController.php
+++ carpeta_proyectito/app/Http/Controllers/FabricanteController.php
@@ -7,105 +7,176 @@ use Illuminate\Http\Response;
 
 use App\Fabricante;
 
-class FabricanteController extends Controller {
+class FabricanteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['status'=>'ok','data'=>Fabricante::all()], 200);
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		// Devolverá todos los fabricantes.
-		// return "Mostrando todos los fabricantes de la base de datos.";
-		// return Fabricante::all();  No es lo más correcto por que se devolverían todos los registros. Se recomienda usar Filtros.
-		// Se debería devolver un objeto con una propiedad como mínimo data y el array de resultados en esa propiedad.
-		// A su vez también es necesario devolver el código HTTP de la respuesta.
-		//php http://elbauldelprogramador.com/buenas-practicas-para-el-diseno-de-una-api-RESTful-pragmatica/
-		// https://cloud.google.com/storage/docs/json_api/v1/status-codes
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return ("muestra formulario de creación de fabricante");
+    }
 
-		return response()->json(['status'=>'ok','data'=>Fabricante::all()], 200);
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		// 
-		return "Se muestra formulario para crear un fabricante.";
+        // Primero comprobaremos si estamos recibiendo todos los campos.
+        if (!$request->input('nombre') || !$request->input('direccion') || !$request->input('telefono'))
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 422 Unprocessable Entity – [Entidad improcesable] Utilizada para errores de validación.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>422,'message'=>'Faltan datos necesarios para el proceso de alta.'])],422);
+        }
 
-	}
+        // Insertamos una fila en Fabricante con create pasándole todos los datos recibidos.
+        // En $request->all() tendremos todos los campos del formulario recibidos.
+        $nuevoFabricante=Fabricante::create($request->all());
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+        // Más información sobre respuestas en http://jsonapi.org/format/
+        // Devolvemos el código HTTP 201 Created – [Creada] Respuesta a un POST que resulta en una creación. Debería ser combinado con un encabezado Location, apuntando a la ubicación del nuevo recurso.
+        return response()->json(['data'=>$nuevoAvion], 201)->header('Location',  url('/api/v1/').'/aviones/'.$nuevoAvion->serie);
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-		// return "Se muestra Fabricante con id: $id";
-		// Buscamos un fabricante por el id.
-		$fabricante=Fabricante::find($id);
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $fabricante=Fabricante::find($id);
 
-		// Si no existe ese fabricante devolvemos un error.
-		if (!$fabricante)
-		{
-			// Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
-			// En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
-			return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra un fabricante con ese código.'])],404);
-		}
+        // Si no existe ese fabricante devolvemos un error.
+        if (!$fabricante)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra un fabricante con ese código.'])],404);
+        }
 
-		return response()->json(['status'=>'ok','data'=>$fabricante],200);
+        return response()->json(['status'=>'ok','data'=>$fabricante],200);
+    }
 
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-		return "Se muestra formulario para editar Fabricante con id: $id";
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Comprobamos si el fabricante que nos están pasando existe o no.
+        $fabricante=Fabricante::find($id);
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+        // Si no existe ese fabricante devolvemos un error.
+        if (!$fabricante)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+            // En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+            return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra un fabricante con ese código.'])],404);
+        }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+        // Listado de campos recibidos teóricamente.
+        $nombre=$request->input('nombre');
+        $direccion=$request->input('direccion');
+        $telefono=$request->input('telefono');
 
+        // Necesitamos detectar si estamos recibiendo una petición PUT o PATCH.
+        // El método de la petición se sabe a través de $request->method();
+        if ($request->method() === 'PATCH')
+        {
+            // Creamos una bandera para controlar si se ha modificado algún dato en el método PATCH.
+            $bandera = false;
+
+            // Actualización parcial de campos.
+            if ($nombre)
+            {
+                $fabricante->nombre = $nombre;
+                $bandera=true;
+            }
+
+            if ($direccion)
+            {
+                $fabricante->direccion = $direccion;
+                $bandera=true;
+            }
+
+
+            if ($telefono)
+            {
+                $fabricante->telefono = $telefono;
+                $bandera=true;
+            }
+
+            if ($bandera)
+            {
+                // Almacenamos en la base de datos el registro.
+                $fabricante->save();
+                return response()->json(['status'=>'ok','data'=>$fabricante], 200);
+            }
+            else
+            {
+                // Se devuelve un array errors con los errores encontrados y cabecera HTTP 304 Not Modified – [No Modificada] Usado cuando el cacheo de encabezados HTTP está activo
+                // Este código 304 no devuelve ningún body, así que si quisiéramos que se mostrara el mensaje usaríamos un código 200 en su lugar.
+                return response()->json(['errors'=>array(['code'=>304,'message'=>'No se ha modificado ningún dato de fabricante.'])],304);
+            }
+        }
+
+
+        // Si el método no es PATCH entonces es PUT y tendremos que actualizar todos los datos.
+        if (!$nombre || !$direccion || !$telefono)
+        {
+            // Se devuelve un array errors con los errores encontrados y cabecera HTTP 422 Unprocessable Entity – [Entidad improcesable] Utilizada para errores de validación.
+            return response()->json(['errors'=>array(['code'=>422,'message'=>'Faltan valores para completar el procesamiento.'])],422);
+        }
+
+        $fabricante->nombre = $nombre;
+        $fabricante->direccion = $direccion;
+        $fabricante->telefono = $telefono;
+
+        // Almacenamos en la base de datos el registro.
+        $fabricante->save();
+        return response()->json(['status'=>'ok','data'=>$fabricante], 200);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       //
+    }
 }
