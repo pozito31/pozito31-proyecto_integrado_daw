71b7bdc396b1c2f262491626aea13e6a9ed257bd
pozito31 <jessicapozo198715@gmail.com>
2020-04-09 15:41:37 +0200

    comentando una linea del fabricanteController

--- carpeta_proyectito/app/Http/Controllers/FabricanteController.php
+++ carpeta_proyectito/app/Http/Controllers/FabricanteController.php
@@ -19,7 +19,7 @@ class FabricanteController extends Controller
 {
     public function __construct()
 	{
-		$this->middleware('auth.basic',['only'=>['store','update','destroy']]);
+		/*$this->middleware('auth.basic',['only'=>['store','update','destroy']]);*/
     }
     
     /**
