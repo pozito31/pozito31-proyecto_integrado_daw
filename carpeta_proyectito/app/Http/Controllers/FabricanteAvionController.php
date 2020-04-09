be68859668b11425ebcccf0513f93df694ff5324
pozito31 <jessicapozo198715@gmail.com>
2020-04-09 16:46:44 +0200

    mofificacion FabricanteAvionController

--- carpeta_proyectito/app/Http/Controllers/FabricanteAvionController.php
+++ carpeta_proyectito/app/Http/Controllers/FabricanteAvionController.php
@@ -2,90 +2,99 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
+use App\Fabricante;
+use App\Avion;
 
-class FabricanteAvionController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index($idFabricante)
-    {
-        // Devolverá todos los aviones.
-		return "Mostrando los aviones del fabricante con Id $idFabricante";
-    }
+class FabricanteAvionController extends Controller {
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create($idFabricante)
-    {
-        //
-        return "Se muestra formulario para crear un avión del fabricante $idFabricante.";
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($idFabricante)
+	{
+		// Devolverá todos los aviones.
+		//return "Mostrando los aviones del fabricante con Id $idFabricante";
+		$fabricante=Fabricante::find($idFabricante);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+		if (! $fabricante)
+		{
+			// Se devuelve un array errors con los errores encontrados y cabecera HTTP 404.
+			// En code podríamos indicar un código de error personalizado de nuestra aplicación si lo deseamos.
+			return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra un fabricante con ese código.'])],404);
+		}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($idFabricante,$idAvion)
-    {
-        //
-        return "Se muestra avión $idAvion del fabricante $idFabricante";
-    }
+		return response()->json(['status'=>'ok','data'=>$fabricante->aviones()->get()],200);
+		//return response()->json(['status'=>'ok','data'=>$fabricante->aviones],200);
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($idFabricante,$idAvion)
-    {
-        //
-        return "Se muestra formulario para editar el avión $idAvion del fabricante $idFabricante";
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create($idFabricante)
+	{
+		//
+		return "Se muestra formulario para crear un avión del fabricante $idFabricante.";
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $idFabricante,$idAvion)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($idFabricante,$idAvion)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($idFabricante,$idAvion)
+	{
+		//
+		return "Se muestra avión $idAvion del fabricante $idFabricante";
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($idFabricante,$idAvion)
+	{
+		//
+		return "Se muestra formulario para editar el avión $idAvion del fabricante $idFabricante";
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($idFabricante,$idAvion)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($idFabricante,$idAvion)
+	{
+		//
+	}
+
 }
