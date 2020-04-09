5a0d93eed405c325f902734dda2daa0d33a0e854
pozito31 <jessicapozo198715@gmail.com>
2020-04-09 15:17:36 +0200

    terminado de arreglar el fichero rutas api.php

--- carpeta_proyectito/routes/api.php
+++ carpeta_proyectito/routes/api.php
@@ -19,14 +19,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // resource recibe nos parámetros(URI del recurso, Controlador que gestionará las peticiones)
-Route::resource('fabricantes','FabricanteController');   // Todos los métodos menos Edit que mostraría un formulario de edición.
+Route::resource('fabricantes','FabricanteController',['except'=>['edit','create'] ]);   // Todos los métodos menos Edit que mostraría un formulario de edición.
 
 // Si queremos dar  la funcionalidad de ver todos los aviones tendremos que crear una ruta específica.
 // Pero de aviones solamente necesitamos solamente los métodos index y show.
 // Lo correcto sería hacerlo así:
-Route::resource('aviones','AvionController'); 
+Route::resource('aviones','AvionController',[ 'only'=>['index','show'] ]); // El resto se gestionan en FabricanteAvionController
 
 // Como la clase principal es fabricantes y un avión no se puede crear si no le indicamos el fabricante,
 // entonces necesitaremos crear lo que se conoce como  "Recurso Anidado" de fabricantes con aviones.
 // Definición del recurso anidado:
-Route::resource('fabricantes.aviones','FabricanteAvionController');
+Route::resource('fabricantes.aviones','FabricanteAvionController',[ 'except'=>['show','edit','create'] ]);
+
