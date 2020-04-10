a9b03566b671b784789c553da46b74af6de6e940
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 12:15:24 +0200

    seguimos modificando

--- carpeta_proyectito/public/index.php
+++ carpeta_proyectito/public/index.php
@@ -73,28 +73,54 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 */
 
+ 
 
+ 
 
+ 
+
+ 
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+ 
 
+ 
 
+ 
+
+ 
 
 $response = $kernel->handle(
 
+ 
+
+ 
 
     $request = Illuminate\Http\Request::capture()
 
+ 
+
+ 
 
 );
 
+ 
 
+ 
 
+ 
+
+ 
 
 $response->send();
 
+ 
 
+ 
 
+ 
+
+ 
 
 $kernel->terminate($request, $response);
