d32cb3678661d50695d940dc47d4aa88e102c170
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 17:05:20 +0200

    Agregando contenido a la migracion aviones

--- carpeta_ejemplo/database/migrations/2020_04_10_145823_aviones_migration.php
+++ carpeta_ejemplo/database/migrations/2020_04_10_145823_aviones_migration.php
@@ -13,11 +13,28 @@ class AvionesMigration extends Migration
      */
     public function up()
     {
-        Schema::create('aviones', function (Blueprint $table) {
-            $table->id();
+        Schema::create('aviones', function(Blueprint $table)
+        {
+            $table->increments('serie');
+            $table->string('modelo');
+            $table->float('longitud');
+            $table->integer('capacidad');
+            $table->integer('velocidad');
+            $table->integer('alcance');
+
+            // Añadimos la clave foránea con Fabricante. fabricante_id
+            // Acordarse de añadir al array protected $fillable del fichero de modelo "Avion.php" la nueva columna:
+            // protected $fillable = array('modelo','longitud','capacidad','velocidad','alcance','fabricante_id');
+            $table->integer('fabricante_id')->unsigned();
+
+            // Indicamos cual es la clave foránea de esta tabla:
+            $table->foreign('fabricante_id')->references('id')->on('fabricantes');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
