6ed210c5bb46882e932f671174b530450d8cc3ff
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 18:08:23 +0200

    Agregando datos a la migracion de fabricantes

--- carpeta_ejemplo/database/migrations/2020_04_10_160329_fabricantes_migration.php
+++ carpeta_ejemplo/database/migrations/2020_04_10_160329_fabricantes_migration.php
@@ -6,26 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class FabricantesMigration extends Migration
 {
-    /**
+   /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
     {
-        Schema::create('fabricantes', function (Blueprint $table) {
-            $table->id();
+        Schema::create('fabricantes', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('telefono');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    * Reverse the migrations.
+    *
+    * @return void
+    */
     public function down()
-    {
+    {   
         Schema::dropIfExists('fabricantes');
     }
 }
