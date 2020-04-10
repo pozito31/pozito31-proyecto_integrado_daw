e25530d51ba921eba42ae0122b5b1fef1cfc94ee
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 17:26:25 +0200

    Agregando contenido a FabricanteSeeder

--- carpeta_ejemplo/database/seeds/FabricanteSeeder.php
+++ carpeta_ejemplo/database/seeds/FabricanteSeeder.php
@@ -2,15 +2,35 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Fabricante;
+
+use Faker\Factory as Faker;
+
 class FabricanteSeeder extends Seeder
 {
-    /**
+     /**
      * Run the database seeds.
      *
      * @return void
      */
+
     public function run()
     {
-        //
+        // Creamos una instancia de Faker
+        $faker = Faker::create();
+
+        // Creamos un bucle para cubrir 5 fabricantes:
+        for ($i=1; $i<=5; $i++)
+        {
+            // Cuando llamamos al método create del Modelo Fabricante
+            // se está creando una nueva fila en la tabla.
+            Fabricante::create(
+                [
+                    'nombre'=>$faker->name(),
+                    'direccion'=>$faker->streetName(),
+                    'telefono'=>$faker->phoneNumber()
+                ]
+            );
+        }
     }
 }
