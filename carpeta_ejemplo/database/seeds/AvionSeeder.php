b8e113204d08387daf3e19a5dbe797e6e67d76bd
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 18:28:07 +0200

    Agregando contenido al Seeder de Avion

--- carpeta_ejemplo/database/seeds/AvionSeeder.php
+++ carpeta_ejemplo/database/seeds/AvionSeeder.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
+// Hace uso del modelo de Fabricante.
+use App\Fabricante;
+
+// Hace uso del modelo de Avion.
+use App\Avion;
+
+// Le indicamos que utilice también Faker.
+// Información sobre Faker: https://github.com/fzaninotto/Faker
+use Faker\Factory as Faker;
+
 class AvionSeeder extends Seeder
 {
     /**
@@ -9,8 +19,32 @@ class AvionSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+   public function run()
+   {
+       // Creamos una instancia de Faker
+       $faker = Faker::create();
+
+       // Para cubrir los aviones tenemos que tener en cuenta qué fabricantes tenemos.
+       // Para que la clave foránea no nos de problemas.
+       // Averiguamos cuantos fabricantes hay en la tabla.
+       $cuantos= Fabricante::all()->count();
+
+       // Creamos un bucle para cubrir 20 aviones:
+       for ($i=1; $i<=20; $i++)
+       {
+           // Cuando llamamos al método create del Modelo Avion
+           // se está creando una nueva fila en la tabla.
+           Avion::create(
+               [
+                'modelo'=>$faker->word(),
+                'longitud'=>$faker->randomFloat(2,10,150),
+                'capacidad'=>$faker->randomNumber(3),  // de 3 dígitos como máximo.
+                'velocidad'=>$faker->randomNumber(4),  // de 4 dígitos como máximo.
+                'alcance'=>$faker->randomNumber(),
+                'fabricante_id'=>$faker->numberBetween(1,$cuantos)
+               ]
+           );
+       }
+
+   }
 }
