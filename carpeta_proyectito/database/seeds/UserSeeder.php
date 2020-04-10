5509010474017382e960c919c52bbdb352a337c0
pozito31 <jessicapozo198715@gmail.com>
2020-04-10 11:09:01 +0200

    editando fichero UserSeeder.php

--- carpeta_proyectito/database/seeds/UserSeeder.php
+++ carpeta_proyectito/database/seeds/UserSeeder.php
@@ -2,15 +2,23 @@
 
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+// Hace uso del modelo de User.
+use App\User;
+
+class UserSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		User::create(
+			[
+				'email'=>'test@test.es',
+				'password'=> Hash::make('abc123')	//Cifrado de la contraseña abc123
+			]);
+
+	}
 }
