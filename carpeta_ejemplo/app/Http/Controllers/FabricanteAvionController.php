b92df44b61d9e079f5e65c3ec715086d40977660
pozito31 <jessicapozo198715@gmail.com>
2020-04-11 12:11:53 +0200

    Añadiendo cosas al controlador FabricanteAvion

--- carpeta_ejemplo/app/Http/Controllers/FabricanteAvionController.php
+++ carpeta_ejemplo/app/Http/Controllers/FabricanteAvionController.php
@@ -2,83 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class FabricanteAvionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+   /**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($idFabricante)
+	{
+		// Devolverá todos los aviones.
+		return "Mostrando los aviones del fabricante con Id $idFabricante";
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create($idFabricante)
+	{
+		//
+		return "Se muestra formulario para crear un avión del fabricante $idFabricante.";
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($idFabricante,$idAvion)
+	{
+		//
+		return "Se muestra avión $idAvion del fabricante $idFabricante";
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($idFabricante,$idAvion)
+	{
+		//
+		return "Se muestra formulario para editar el avión $idAvion del fabricante $idFabricante";
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($idFabricante,$idAvion)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($idFabricante,$idAvion)
+	{
+		//
+	}
 }
