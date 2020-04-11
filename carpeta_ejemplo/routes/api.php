b3384132a9ab9ca309eaeaefdfe4952131e36031
pozito31 <jessicapozo198715@gmail.com>
2020-04-11 12:46:54 +0200

    editanto fichero api.php

--- carpeta_ejemplo/routes/api.php
+++ carpeta_ejemplo/routes/api.php
@@ -18,15 +18,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// resource recibe nos parámetros(URI del recurso, Controlador que gestionará las peticiones)
-Route::resource('fabricantes','FabricanteController',['except'=>['edit','create'] ]);   // Todos los métodos menos Edit que mostraría un formulario de edición.
+// Versionado de la API.
+Route::prefix('v1')->group(function () {
 
-// Si queremos dar  la funcionalidad de ver todos los aviones tendremos que crear una ruta específica.
-// Pero de aviones solamente necesitamos solamente los métodos index y show.
-// Lo correcto sería hacerlo así:
-Route::resource('aviones','AvionController',[ 'only'=>['index','show'] ]); // El resto se gestionan en FabricanteAvionController
+    // resource recibe nos parámetros(URI del recurso, Controlador que gestionará las peticiones)
+    Route::resource('fabricantes','FabricanteController',['except'=>['edit','create'] ]);   // Todos los métodos menos Edit que mostraría un formulario de edición.
 
-// Como la clase principal es fabricantes y un avión no se puede crear si no le indicamos el fabricante,
-// entonces necesitaremos crear lo que se conoce como  "Recurso Anidado" de fabricantes con aviones.
-// Definición del recurso anidado:
-Route::resource('fabricantes.aviones','FabricanteAvionController',[ 'except'=>['show','edit','create'] ]);
+    // Si queremos dar  la funcionalidad de ver todos los aviones tendremos que crear una ruta específica.
+    // Pero de aviones solamente necesitamos solamente los métodos index y show.
+    // Lo correcto sería hacerlo así:
+    Route::resource('aviones','AvionController',[ 'only'=>['index','show'] ]); // El resto se gestionan en FabricanteAvionController
+
+    // Como la clase principal es fabricantes y un avión no se puede crear si no le indicamos el fabricante,
+    // entonces necesitaremos crear lo que se conoce como  "Recurso Anidado" de fabricantes con aviones.
+    // Definición del recurso anidado:
+    Route::resource('fabricantes.aviones','FabricanteAvionController',[ 'except'=>['show','edit','create'] ]);
+});
