bfdf90b1ff48650378cfdac8fe5a19a88ad52311
pozito31 <jessicapozo198715@gmail.com>
2020-04-12 17:20:24 +0200

    agregado el componente cabecero

--- control-clientes/src/app/app.module.ts
+++ control-clientes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent
   ],
   imports: [
     BrowserModule,
---
+++ control-clientes/src/app/componentes/cabecero/cabecero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecero',
+  templateUrl: './cabecero.component.html',
+  styleUrls: ['./cabecero.component.css']
+})
+export class CabeceroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
