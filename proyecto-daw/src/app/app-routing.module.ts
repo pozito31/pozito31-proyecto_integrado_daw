0ddb12327f4474956e4016c83023fc288e07ed6d
pozito31 <jessicapozo198715@gmail.com>
2020-04-12 14:17:35 +0200

    agregando a app-routing el componente historia

--- proyecto-daw/src/app/app-routing.module.ts
+++ proyecto-daw/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HistoriaComponent} from './historia/historia.component';
 
 const routes: Routes = [
-  
+  { path: 'historia', component: HistoriaComponent}
 ];
 
 @NgModule({
