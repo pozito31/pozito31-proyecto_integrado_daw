83d4f31777587f6f2b0bdcfc39e9af60ed3d0156
pozito31 <jessicapozo198715@gmail.com>
2020-04-12 14:54:59 +0200

    modificando

--- proyecto-daw/src/app/app-routing.module.ts
+++ proyecto-daw/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HistoriaComponent} from './historia/historia.component';
 
 const routes: Routes = [
-  { path: 'historia/historia.component.html', component: HistoriaComponent}
+  { path: 'historia', component: HistoriaComponent}
 ];
 
 @NgModule({
