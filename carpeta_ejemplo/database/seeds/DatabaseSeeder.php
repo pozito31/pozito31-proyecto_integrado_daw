8f0ebf4bb0b3a675e70143e0fe978c0173a144d8
pozito31 <jessicapozo198715@gmail.com>
2020-04-12 11:30:38 +0200

    agregando una cosa a DatabaseSeeder.php

--- carpeta_ejemplo/database/seeds/DatabaseSeeder.php
+++ carpeta_ejemplo/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
 		Model::unguard();
 
 		$this->call('FabricanteSeeder');
-	   	$this->call('AvionSeeder');
+		$this->call('AvionSeeder');
+		$this->call('UserSeeder');
 	}
 }
