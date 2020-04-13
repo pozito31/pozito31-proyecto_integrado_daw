fd4dce499ee83792e78a575e5d668dd6fa58e06e
pozito31 <jessicapozo198715@gmail.com>
2020-04-13 15:32:09 +0200

    modificando fichero para las rutas

--- front-end-proyecto/src/app/app-routing.module.ts
+++ front-end-proyecto/src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { HistoriaComponent } from './componentes/historia/historia.component';
+import { HermanomayorComponent } from './componentes/hermanomayor/hermanomayor.component';
+import { NovenasComponent } from './componentes/novenas/novenas.component';
+import { ParrocoComponent } from './componentes/parroco/parroco.component';
+import { NoticiasComponent } from './componentes/noticias/noticias.component';
+import { NuevohermanoComponent } from './componentes/nuevohermano/nuevohermano.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
+import { VideosComponent } from './componentes/videos/videos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'historia', component: HistoriaComponent},
+  {path: 'hermanomayor', component: HermanomayorComponent},
+  {path: 'novenas', component: NovenasComponent},
+  {path: 'parroco', component: ParrocoComponent},
+  {path: 'noticias', component: NoticiasComponent},
+  {path: 'nuevohermano', component: NuevohermanoComponent},
+  {path: 'imagenes', component: ImagenesComponent},
+  {path: 'videos', component: VideosComponent},
+  {path: 'contacto', component: ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
