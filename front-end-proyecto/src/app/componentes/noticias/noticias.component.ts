94208fb90cd52f27d1737644c9ea8de0c73df71b
pozito31 <jessicapozo198715@gmail.com>
2020-04-15 10:58:07 +0200

    cosas de los formularios

--- front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
+++ front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-noticias',
@@ -19,18 +19,43 @@ export class NoticiasComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get tituloNoValido(){
+    return this.forma.get('titulo').invalid && this.forma.get('titulo').touched
+  }
+
+  get descripcionNoValida(){
+    return this.forma.get('descripcion').invalid && this.forma.get('descripcion').touched
+  }
+
+  get textoNoValido(){
+    return this.forma.get('texto').invalid && this.forma.get('texto').touched
+  }
+
+  get imagenNoValida(){
+    return this.forma.get('imagen').invalid && this.forma.get('imagen').touched
+  }
+
   crearFormulario(){
     
     this.forma = this.fb.group({
-      titulo: [''],
-      descripcion: [''],
-      texto: [''],
-      imagen: ['']
+      titulo: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      texto: ['', Validators.required],
+      imagen: ['', Validators.required]
     });
   }
 
   insertar(){
     console.log( this.forma );
+
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+
+    //Posteo de información
+    this.forma.reset();
   }
 
 }
