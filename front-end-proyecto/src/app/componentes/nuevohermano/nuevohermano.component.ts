8684d733f15c767b577ce21e9fe997aa8f95af59
pozito31 <jessicapozo198715@gmail.com>
2020-04-16 17:57:19 +0200

    agregando cosas al componente nuevohermano

--- front-end-proyecto/src/app/componentes/nuevohermano/nuevohermano.component.ts
+++ front-end-proyecto/src/app/componentes/nuevohermano/nuevohermano.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevohermano',
@@ -9,7 +10,27 @@ export class NuevohermanoComponent implements OnInit {
 
   constructor() { }
 
+  NuevoHermanoFormulario: FormGroup;
+
   ngOnInit(): void {
+    this.NuevoHermanoFormulario = new FormGroup({
+      'nombre': new FormControl('', [
+         Validators.required,
+         Validators.minLength(4)
+      ]),
+      'primerapellido': new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  get nombre() { return this.NuevoHermanoFormulario.get('nombre') }
+  get primerapellido() { return this.NuevoHermanoFormulario.get('primerapellido') }
+
+  onSubmit() {
+    const valor = this.NuevoHermanoFormulario;
+    console.log(valor);
   }
 
 }
