e3dd882161526116049535f6cb4bd6472bc7581b
pozito31 <jessicapozo198715@gmail.com>
2020-04-16 17:09:16 +0200

    arreglado

--- front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
+++ front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-noticias',
@@ -11,7 +12,20 @@ export class NoticiasComponent implements OnInit {
 
   constructor() {}
 
+  ContactoFormulario: FormGroup;
+
   ngOnInit(): void {
+    this.ContactoFormulario = new FormGroup({
+      'email': new FormControl('', [
+         Validators.required
+      ]),
+      'nombre': new FormControl('', [
+        Validators.required
+      ]),
+      'mensaje': new FormControl('', [
+        Validators.required
+      ])
+    });
   }
-  
+
 }
