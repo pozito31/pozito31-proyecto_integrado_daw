cd8ce01ba22018e976ad50c335e969abd8e155e8
pozito31 <jessicapozo198715@gmail.com>
2020-04-16 15:47:31 +0200

    modificando informacion

--- front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
+++ front-end-proyecto/src/app/componentes/noticias/noticias.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-noticias',
@@ -8,54 +7,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class NoticiasComponent implements OnInit {
 
-  forma: FormGroup;
 
-  constructor( private fb: FormBuilder) {
 
-    this.crearFormulario();
-
-   }
+  constructor() {}
 
   ngOnInit(): void {
   }
-
-  get tituloNoValido(){
-    return this.forma.get('titulo').invalid && this.forma.get('titulo').touched
-  }
-
-  get descripcionNoValida(){
-    return this.forma.get('descripcion').invalid && this.forma.get('descripcion').touched
-  }
-
-  get textoNoValido(){
-    return this.forma.get('texto').invalid && this.forma.get('texto').touched
-  }
-
-  get imagenNoValida(){
-    return this.forma.get('imagen').invalid && this.forma.get('imagen').touched
-  }
-
-  crearFormulario(){
-    
-    this.forma = this.fb.group({
-      titulo: ['', Validators.required],
-      descripcion: ['', Validators.required],
-      texto: ['', Validators.required],
-      imagen: ['', Validators.required]
-    });
-  }
-
-  insertar(){
-    console.log( this.forma );
-
-    if(this.forma.invalid){
-      return Object.values(this.forma.controls).forEach(control => {
-        control.markAsTouched();
-      });
-    }
-
-    //Posteo de información
-    this.forma.reset();
-  }
-
+  
 }
