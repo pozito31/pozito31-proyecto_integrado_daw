aff12a4a87fff9c60851e4ca96037c93848aa176
pozito31 <jessicapozo198715@gmail.com>
2020-04-17 16:16:33 +0200

    actualizado los datos de nuevohermano

--- front-end-proyecto/src/app/componentes/nuevohermano/nuevohermano.component.ts
+++ front-end-proyecto/src/app/componentes/nuevohermano/nuevohermano.component.ts
@@ -74,6 +74,58 @@ export class NuevohermanoComponent implements OnInit {
       'profesion': new FormControl('', [
         Validators.required,
         Validators.minLength(4)
+      ]),
+      'metododepago': new FormControl('', [
+        Validators.required
+      ]),
+      'iban': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'tipodecuota': new FormControl('', [
+        Validators.required
+      ]),
+      'nombredeltitularbancario': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'domiciliodeltitular': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'dnideltitular': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'titulardelatarjetadecredito': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'pais': new FormControl('', [
+        Validators.required
+      ]),
+      'comunidadoestado': new FormControl('', [
+        Validators.required
+      ]),
+      'provincia': new FormControl('', [
+        Validators.required
+      ]),
+      'poblacion': new FormControl('', [
+        Validators.required
+      ]),
+      'direccion': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'codigopostal': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'foto': new FormControl('', [
+        Validators.required
+      ]),
+      'acepto': new FormControl('', [
+        Validators.required
       ])
     });
   }
@@ -94,6 +146,22 @@ export class NuevohermanoComponent implements OnInit {
   get telefonosecundario() { return this.NuevoHermanoFormulario.get('telefonosecundario') }
   get correo() { return this.NuevoHermanoFormulario.get('correo') }
   get profesion() { return this.NuevoHermanoFormulario.get('profesion') }
+  get metododepago() { return this.NuevoHermanoFormulario.get('metododepago') }
+  get iban() { return this.NuevoHermanoFormulario.get('iban') }
+  get tipodecuota() { return this.NuevoHermanoFormulario.get('tipodecuota') }
+  get nombredeltitularbancario() { return this.NuevoHermanoFormulario.get('nombredeltitularbancario') }
+  get domiciliodeltitular() { return this.NuevoHermanoFormulario.get('domiciliodeltitular') }
+  get dnideltitular() { return this.NuevoHermanoFormulario.get('dnideltitular') }
+  get titulardelatarjetadecredito() { return this.NuevoHermanoFormulario.get('titulardelatarjetadecredito') }
+  get pais() { return this.NuevoHermanoFormulario.get('pais') }
+  get comunidadoestado() { return this.NuevoHermanoFormulario.get('comunidadoestado') }
+  get provincia() { return this.NuevoHermanoFormulario.get('provincia') }
+  get poblacion() { return this.NuevoHermanoFormulario.get('poblacion') }
+  get direccion() { return this.NuevoHermanoFormulario.get('direccion') }
+  get codigopostal() { return this.NuevoHermanoFormulario.get('codigopostal') }
+  get foto() { return this.NuevoHermanoFormulario.get('foto') }
+  get acepto() { return this.NuevoHermanoFormulario.get('acepto') }
+
 
   onSubmit() {
     const valor = this.NuevoHermanoFormulario;
