209b9372e562fb40338d2654a06ffa46d6d6322d
pozito31 <jessicapozo198715@gmail.com>
2020-04-18 12:29:59 +0200

    actualizando datos

--- front-end-proyecto/src/app/componentes/login/login.component.ts
+++ front-end-proyecto/src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,28 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  LoginFormulario: FormGroup;
+
   ngOnInit(): void {
+    this.LoginFormulario = new FormGroup({
+      'usuario': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'password': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(15)
+      ])
+    });
+  }
+
+  get usuario() { return this.LoginFormulario.get('usuario') }
+  get password() { return this.LoginFormulario.get('password') }
+
+  onSubmit() {    
+    const val = this.LoginFormulario.value;
+    console.log(val);
   }
 
 }
