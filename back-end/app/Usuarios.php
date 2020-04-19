a576da656d5d7c250ec458c8192400885bfffc6c
pozito31 <jessicapozo198715@gmail.com>
2020-04-19 15:28:42 +0200

    agregando cosas a usuarios

--- back-end/app/Usuarios.php
+++ back-end/app/Usuarios.php
@@ -11,13 +11,13 @@ class Usuarios extends Model
 
     // Eloquent asume que cada tabla tiene una clave primaria con una columna llamada id.
     // Si éste no fuera el caso entonces hay que indicar cuál es nuestra clave primaria en la tabla:
-    protected $primaryKey = 'Id_usuario';
+    protected $primaryKey = 'id_usuario';
 
     //Atributos que se pueden asignar de manera masiva.
-    protected $fillable = array('Nombre', 'Apellidos', 'Fecha_alta', 'usuario', 'password');
+    protected $fillable = array('nombre', 'apellidos', 'fecha_alta', 'usuario', 'password');
 
     //Aqui ponemos los campos que no queremos que se devuelvan en las consultas.
     protected $hidden = ['created_at', 'updated_at'];
 
-    
+
 }
