9b28d40f5983e538dec7eb8ded5d45b52e040e29
pozito31 <jessicapozo198715@gmail.com>
2020-04-20 13:43:29 +0200

    agregando datos a migracion pagos

--- back-end/database/migrations/2020_04_20_103308_pagos_migration.php
+++ back-end/database/migrations/2020_04_20_103308_pagos_migration.php
@@ -14,8 +14,21 @@ class PagosMigration extends Migration
     public function up()
     {
         Schema::create('pagos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+           $table->increments('id_pago');
+           $table->string('cantidad');
+           $table->string('realizar_pago');
+           $table->string('fecha_transaccion');
+
+           //Añadimos la clave foránea con Usuario. usuario_id_usuario
+           //Acordarse de añadir al array protected $fillable del fichero de modelo "Pagos.php" la nueva columna:
+           //protected $fillable = array('estado', 'tipo', 'nombre', 'situacion', usuarios_id_usuario);
+           $table->integer('usuarios_id_usuario')->unsigned();
+
+           // Indicamos cual es la clave foránea de esta tabla:
+           $table->foreign('usuarios_id_usuario')->references('id_usuario')->on('usuarios');
+
+           // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
+           $table->timestamps();
         });
     }
 
