54371c7315b3c333c64be4dd492174e846c25e6a
pozito31 <jessicapozo198715@gmail.com>
2020-04-20 09:20:48 +0200

    modificando modelos

--- back-end/app/Pagos.php
+++ back-end/app/Pagos.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pagos extends Model
 {
-    //
+    ////Nombre de la tabla en MySQL
+    protected $table='pagos';
+
+    // Eloquent asume que cada tabla tiene una clave primaria con una columna llamada id.
+    // Si éste no fuera el caso entonces hay que indicar cuál es nuestra clave primaria en la tabla:
+    protected $primaryKey = 'id_pago';
+
+    //Atributos que se pueden asignar de manera masiva.
+    protected $fillable = array('cantidad', 'realizar_pago', 'fecha_transaccion');
+
+    //Aqui ponemos los campos que no queremos que se devuelvan en las consultas.
+    protected $hidden = ['created_at', 'updated_at'];
+
+    //Relación de Pagos con Usuarios:
+    public function usuarios()
+    {
+        return $this->belongsTo('App\Usuarios');
+    }
 }
