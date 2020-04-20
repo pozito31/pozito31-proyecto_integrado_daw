1a471459b9ed88616643573cffc3cbc77878f7a2
pozito31 <jessicapozo198715@gmail.com>
2020-04-20 14:57:54 +0200

    Agregando informacion a DatabaseSeeder

--- back-end/database/seeds/DatabaseSeeder.php
+++ back-end/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Model::unguard();
+
+        $this->call('UsuariosSeeder');
+        $this->call('PagosSeeder');
+        $this->call('RolesSeeder');
+        $this->call('NoticiasSeeder');
+        $this->call('ImagenesSeeder');
     }
 }
