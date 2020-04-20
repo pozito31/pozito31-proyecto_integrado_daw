bb9c9a5ef943bc414702549e4d577ef6d92beb1c
pozito31 <jessicapozo198715@gmail.com>
2020-04-20 14:02:55 +0200

    agregando datos a migracion noticias

--- back-end/database/migrations/2020_04_20_103650_noticias_migration.php
+++ back-end/database/migrations/2020_04_20_103650_noticias_migration.php
@@ -14,7 +14,19 @@ class NoticiasMigration extends Migration
     public function up()
     {
         Schema::create('noticias', function (Blueprint $table) {
-            $table->id();
+           $table->increments('id_noticia');
+           $table->string('descripcion');
+           $table->string('directorio_noticia');
+
+            //Añadimos la clave foránea con Usuario. usuario_id_usuario
+           //Acordarse de añadir al array protected $fillable del fichero de modelo "Noticias.php" la nueva columna:
+           //protected $fillable = array('descripcion', 'directorio_noticia', usuarios_id_usuario);
+            $table->integer('usuarios_id_usuario')->unsigned();
+
+           // Indicamos cual es la clave foránea de esta tabla:
+            $table->foreign('usuarios_id_usuario')->references('id_usuario')->on('usuarios');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
