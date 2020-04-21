758888babe5088ef24fd7bd9b101c53831159c35
pozito31 <jessicapozo198715@gmail.com>
2020-04-21 15:32:29 +0200

    agregando datos

--- back-end/database/seeds/RolesSeeder.php
+++ back-end/database/seeds/RolesSeeder.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
+//Hace uso del modelo de Usuarios.
+use App\Usuarios;
+
+//Hace uso del modelo de Roles.
+use App\Roles;
+
+// Le indicamos que utilice también Faker.
+// Información sobre Faker: https://github.com/fzaninotto/Faker
+use Faker\Factory as Faker;
+
 class RolesSeeder extends Seeder
 {
     /**
@@ -11,6 +21,14 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Creamos una instancia de Faker
+        $faker = Faker::create();
+
+        //Para cubrir los pagos tenemos que tener en cuenta que usuarios tenemos.
+        //Para que la clave foránea no nos de problemas.
+        //Averiguamos cuantos usuarios hay en la tabla.
+        $cuantos= Usuarios::all()->count();
+
+        
     }
 }
