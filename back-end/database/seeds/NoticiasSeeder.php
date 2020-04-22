513b3303e5590ace7be060f5dbecfd72ee577b22
pozito31 <jessicapozo198715@gmail.com>
2020-04-22 14:48:11 +0200

    agregando datos a NoticiasSeeder

--- back-end/database/seeds/NoticiasSeeder.php
+++ back-end/database/seeds/NoticiasSeeder.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
+//Hace uso del modelo de Usuarios.
+use App\Usuarios;
+
+//Hace uso del modelo de Noticias.
+use App\Noticias;
+
+// Le indicamos que utilice también Faker.
+// Información sobre Faker: https://github.com/fzaninotto/Faker
+use Faker\Factory as Faker;
+
 class NoticiasSeeder extends Seeder
 {
     /**
@@ -11,6 +21,25 @@ class NoticiasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Creamos una instancia de Faker
+        $faker = Faker::create();
+
+        //Para cubrir los pagos tenemos que tener en cuenta que usuarios tenemos.
+        //Para que la clave foránea no nos de problemas.
+        //Averiguamos cuantos usuarios hay en la tabla.
+        $cuantos= Usuarios::all()->count();
+
+         //Creamos un bucle para cubrir 10 noticias:
+         for ($i=0; $i<=10; $i++){
+            //Cuando llamamos al método create del modelo de Noticias
+            //Se está creando una nueva fila en la tabla.
+            Noticias::create(
+                [
+                    'descripcion'=>$faker->name(),
+                    'directorio_noticia'=>$faker->firstName(),
+                    'usuarios_id_usuario'=>$faker->numberBetween(1,$cuantos)
+                ]
+            );
+        }
     }
 }
