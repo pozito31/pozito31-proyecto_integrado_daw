3576fcc45d870dbb12f70547de0e15346a4f1c4f
pozito31 <jessicapozo198715@gmail.com>
2020-04-22 15:04:58 +0200

    agregando datos a los seeder

--- back-end/database/seeds/NoticiasSeeder.php
+++ back-end/database/seeds/NoticiasSeeder.php
@@ -24,7 +24,7 @@ class NoticiasSeeder extends Seeder
         //Creamos una instancia de Faker
         $faker = Faker::create();
 
-        //Para cubrir los pagos tenemos que tener en cuenta que usuarios tenemos.
+        //Para cubrir las noticias tenemos que tener en cuenta que usuarios tenemos.
         //Para que la clave foránea no nos de problemas.
         //Averiguamos cuantos usuarios hay en la tabla.
         $cuantos= Usuarios::all()->count();
