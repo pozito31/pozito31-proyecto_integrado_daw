84437fb6f347b436fdd6b2d3565fa8228733511c
pozito31 <jessicapozo198715@gmail.com>
2020-04-23 16:19:41 +0200

    agregando datos al modelo Noticias

--- proyecto-integrado-daw/back-end-proyecto/app/Noticias.php
+++ proyecto-integrado-daw/back-end-proyecto/app/Noticias.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Noticias extends Model
 {
-    //
+    //Nombre de la tabla en MySQL
+    protected $table='noticias';
+
+    // Eloquent asume que cada tabla tiene una clave primaria con una columna llamada id.
+    // Si éste no fuera el caso entonces hay que indicar cuál es nuestra clave primaria en la tabla:
+    protected $primaryKey = 'id_noticia';
+
+    //Atributos que se pueden asignar de manera masiva.
+    protected $fillable = array('descripcion', 'directorio_noticia', 'usuarios_id_usuario');
+
+     //Aqui ponemos los campos que no queremos que se devuelvan en las consultas.
+     protected $hidden = ['created_at', 'updated_at'];
+
+     //Relación de Noticias con Usuarios:
+    public function usuarios()
+    {
+        return $this->belongsTo('App\Usuarios');
+    }
+
+    //Relación de Noticias con Imagenes:
+    public function imagenes()
+    {
+        return $this->hasMany('App\Imagenes');
+    }
 }
