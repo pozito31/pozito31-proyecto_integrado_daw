a669990bc57285ee3687e2a2b24fb4b3aef3e49b
pozito31 <jessicapozo198715@gmail.com>
2020-04-23 16:47:58 +0200

    agregando informacion a la migracion usuarios

--- proyecto-integrado-daw/back-end-proyecto/database/migrations/2020_04_23_143913_usuarios_migration.php
+++ proyecto-integrado-daw/back-end-proyecto/database/migrations/2020_04_23_143913_usuarios_migration.php
@@ -14,7 +14,14 @@ class UsuariosMigration extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_usuario');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_alta');
+            $table->string('usuario');
+            $table->string('password');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
