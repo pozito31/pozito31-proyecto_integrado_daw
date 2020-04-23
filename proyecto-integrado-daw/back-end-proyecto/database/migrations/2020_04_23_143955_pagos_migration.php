15b699b94447341bc4be93e99de5e6715dbd76fb
pozito31 <jessicapozo198715@gmail.com>
2020-04-23 16:42:37 +0200

    creando migraciones de las tablas de base de datos

---
+++ proyecto-integrado-daw/back-end-proyecto/database/migrations/2020_04_23_143955_pagos_migration.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PagosMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos');
+    }
+}
