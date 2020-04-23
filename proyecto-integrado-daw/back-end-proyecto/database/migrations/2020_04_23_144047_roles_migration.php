acda0e45c1f3d03cff2ed04b3ebf2e6381b90401
pozito31 <jessicapozo198715@gmail.com>
2020-04-23 16:51:51 +0200

    agregando datos a migration roles

--- proyecto-integrado-daw/back-end-proyecto/database/migrations/2020_04_23_144047_roles_migration.php
+++ proyecto-integrado-daw/back-end-proyecto/database/migrations/2020_04_23_144047_roles_migration.php
@@ -14,7 +14,21 @@ class RolesMigration extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_roles');
+            $table->string('estado');
+            $table->string('tipo');
+            $table->string('nombre');
+            $table->string('situacion');
+
+            //Añadimos la clave foránea con Usuario. usuario_id_usuario
+           //Acordarse de añadir al array protected $fillable del fichero de modelo "Roles.php" la nueva columna:
+           //protected $fillable = array('estado', 'tipo', 'nombre', 'situacion', 'usuarios_id_usuario');
+           $table->integer('usuarios_id_usuario')->unsigned();
+
+            // Indicamos cual es la clave foránea de esta tabla:
+            $table->foreign('usuarios_id_usuario')->references('id_usuario')->on('usuarios');
+
+            // Para que también cree automáticamente los campos timestamps (created_at, updated_at)
             $table->timestamps();
         });
     }
