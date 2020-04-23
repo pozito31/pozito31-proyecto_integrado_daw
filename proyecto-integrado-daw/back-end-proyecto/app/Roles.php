f5baa1f2b35d05eda2122807bea0a66faa4d270b
pozito31 <jessicapozo198715@gmail.com>
2020-04-23 16:14:54 +0200

    agregando datos al modelo Roles

--- proyecto-integrado-daw/back-end-proyecto/app/Roles.php
+++ proyecto-integrado-daw/back-end-proyecto/app/Roles.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Roles extends Model
 {
-    //
+    //Nombre de la tabla en MySQL
+    protected $table='roles';
+
+    // Eloquent asume que cada tabla tiene una clave primaria con una columna llamada id.
+    // Si éste no fuera el caso entonces hay que indicar cuál es nuestra clave primaria en la tabla:
+    protected $primaryKey = 'id_roles';
+
+    //Atributos que se pueden asignar de manera masiva.
+    protected $fillable = array('estado', 'tipo', 'nombre', 'situacion', 'usuarios_id_usuario');
+
+     //Aqui ponemos los campos que no queremos que se devuelvan en las consultas.
+     protected $hidden = ['created_at', 'updated_at'];
+
+     //Relación de Roles con Usuarios:
+    public function usuarios()
+    {
+        return $this->belongsTo('App\Usuarios');
+    }
 }
