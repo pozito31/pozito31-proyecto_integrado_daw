f5e08befe70be4a94685ad170fe9745f311fcae6
pozito31 <jessicapozo198715@gmail.com>
2020-05-01 10:24:06 +0200

    agregando datos

--- proyecto-integrado-daw/back-end-proyecto/app/Http/Controllers/HomeController.php
+++ proyecto-integrado-daw/back-end-proyecto/app/Http/Controllers/HomeController.php
@@ -21,8 +21,12 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        //$request->session()->put(['Jessica'=>'usuario']);
+
+        session(['Juan'=>'administrador']);
+
+        return $request->session()->all();
     }
 }
