fefa996dd0a05a06dec1ec9bdce4632154b63c0e
pozito31 <jessicapozo198715@gmail.com>
2020-05-13 11:50:13 +0200

    agregando rutas a app.routing.modules

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableroComponent } from './pagina/componentes/tablero/tablero.component';
+import { LoginComponent } from './pagina/componentes/login/login.component';
+import { RegistroComponent } from './pagina/componentes/registro/registro.component';
+import { HistoriaComponent } from './pagina/componentes/historia/historia.component';
+import { HermanomayorComponent } from './pagina/componentes/hermanomayor/hermanomayor.component';
+import { NovenasComponent } from './pagina/componentes/novenas/novenas.component';
+import { ParrocoComponent } from './pagina/componentes/parroco/parroco.component';
+import { NoticiasComponent } from './pagina/componentes/noticias/noticias.component';
+import { NuevohermanoComponent } from './pagina/componentes/nuevohermano/nuevohermano.component';
+import { ImagenesComponent } from './pagina/componentes/imagenes/imagenes.component';
+import { VideosComponent } from './pagina/componentes/videos/videos.component';
+import { ContactoComponent } from './pagina/componentes/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{path: '', redirectTo: '/login', pathMatch: 'full'},*/
+  {path: '', component: TableroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'historia', component: HistoriaComponent},
+  {path: 'hermanomayor', component: HermanomayorComponent},
+  {path: 'novenas', component: NovenasComponent},
+  {path: 'parroco', component: ParrocoComponent},
+  {path: 'noticias', component: NoticiasComponent},
+  {path: 'nuevohermano', component: NuevohermanoComponent},
+  {path: 'imagenes', component: ImagenesComponent},
+  {path: 'videos', component: VideosComponent},
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
