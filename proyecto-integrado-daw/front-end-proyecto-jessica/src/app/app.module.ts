380cb3aa2a9a055c7c5e7020bf9401ce0d5130b1
pozito31 <jessicapozo198715@gmail.com>
2020-05-13 10:13:53 +0200

    eliminando

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './pagina/componentes/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule,
