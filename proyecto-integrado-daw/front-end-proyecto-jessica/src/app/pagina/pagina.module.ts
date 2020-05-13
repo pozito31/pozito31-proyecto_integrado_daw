83b5bc283dbd089e394e0fef16184d5a8f195a74
pozito31 <jessicapozo198715@gmail.com>
2020-05-13 12:22:45 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/pagina.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/pagina.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { HermanomayorComponent } from './componentes/hermanomayor/hermanomayor.component';
@@ -15,13 +18,17 @@ import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.componen
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { TableroComponent } from './componentes/tablero/tablero.component';
 import { VideosComponent } from './componentes/videos/videos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [CabeceraComponent, ContactoComponent, HermanomayorComponent, HistoriaComponent, ImagenesComponent, LoginComponent, MenuComponent, NoticiasComponent, NovenasComponent, NuevohermanoComponent, ParrocoComponent, PiePaginaComponent, RegistroComponent, TableroComponent, VideosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ]
 })
 export class PaginaModule { }
