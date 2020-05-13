18d0ad9ef0f8b0e874ec62084c16b26087a9b20e
pozito31 <jessicapozo198715@gmail.com>
2020-05-13 10:16:31 +0200

    modificado

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabeceraComponent } from './pagina/componentes/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CabeceraComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
