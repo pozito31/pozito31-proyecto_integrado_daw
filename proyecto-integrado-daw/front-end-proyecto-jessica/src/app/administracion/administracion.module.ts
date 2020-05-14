2a11a95ac2d40eea5757a9fa2a51b8b8ee738d96
pozito31 <jessicapozo198715@gmail.com>
2020-05-14 10:56:30 +0200

    componente navbar de administración

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/administracion.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './compartidos/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule
   ]
