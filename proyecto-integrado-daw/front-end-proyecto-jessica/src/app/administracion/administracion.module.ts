9f2de5939d44da32fe289115742c55c75c968a34
pozito31 <jessicapozo198715@gmail.com>
2020-05-14 18:41:53 +0200

    añadiendo cosas

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/administracion.module.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './compartidos/navbar/navbar.component';
 import { ListarComponent } from './paginas/noticias/listar/listar.component';
 import { CrearComponent } from './paginas/noticias/crear/crear.component';
 import { EditarComponent } from './paginas/noticias/editar/editar.component';
+import { AdministracionComponent } from './administracion/administracion.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, ListarComponent, CrearComponent, EditarComponent],
+  declarations: [NavbarComponent, ListarComponent, CrearComponent, EditarComponent, AdministracionComponent],
   imports: [
     CommonModule
   ]
