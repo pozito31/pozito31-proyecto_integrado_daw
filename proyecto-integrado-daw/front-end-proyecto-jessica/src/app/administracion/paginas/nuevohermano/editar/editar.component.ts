dc4937ae185782f495bd641537a00bd967be8b63
pozito31 <jessicapozo198715@gmail.com>
2020-05-14 11:16:34 +0200

    crear nuevo hermano para listar, crear y editar

---
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/paginas/nuevohermano/editar/editar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar',
+  templateUrl: './editar.component.html',
+  styleUrls: ['./editar.component.sass']
+})
+export class EditarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/paginas/nuevohermano/listar/listar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar',
+  templateUrl: './listar.component.html',
+  styleUrls: ['./listar.component.sass']
+})
+export class ListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
