3e9c6a57c7324bfa79f4df754e22b4f358c7878c
pozito31 <jessicapozo198715@gmail.com>
2020-05-14 11:25:02 +0200

    componente contacto para listar, crear y editar

---
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/administracion/paginas/contacto/crear/crear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear',
+  templateUrl: './crear.component.html',
+  styleUrls: ['./crear.component.sass']
+})
+export class CrearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
