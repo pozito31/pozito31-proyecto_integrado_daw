19c26026f0dc3f89ee01943655632d898ec3706c
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 14:44:46 +0200

    agregando datos a la interfaz de imagenes

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/imagenes.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/imagenes.ts
@@ -1,2 +1,13 @@
 export interface Imagenes {
+    id_imagen: number;
+    descripcion: string;
+    directorio_foto: string;
+    noticias_id_noticia: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface datosDevueltos  {
+    status: string;
+    data: Imagenes[];
 }
