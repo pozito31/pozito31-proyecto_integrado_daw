f79b048756fa1aa986f342cad3512689c1d17186
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 10:46:05 +0200

    modificando

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
 import { AdministracionComponent } from './administracion/administracion/administracion.component';
-import { NoticiasComponent } from './administracion/noticias/noticias.component';
-import { NuevohermanoComponent } from './administracion/nuevohermano/nuevohermano.component';
-import { ContactoComponent } from './administracion/contacto/contacto.component';
 import { NavbarComponent } from './compartidos/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [AdministracionComponent, NoticiasComponent, NuevohermanoComponent, ContactoComponent, NavbarComponent],
+  declarations: [AdministracionComponent, NavbarComponent],
   imports: [
     CommonModule,
     AdministracionRoutingModule
