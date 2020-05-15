fcb46abd3865e77f6b019562e35b960d070a4706
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 10:42:05 +0200

    agregando componentes a auth

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegistroComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
