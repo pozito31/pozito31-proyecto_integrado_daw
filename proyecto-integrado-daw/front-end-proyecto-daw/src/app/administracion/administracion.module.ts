7b210edbb0fb1fc74cd0e541257e0ea6c04e8451
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 10:48:26 +0200

    agregando componentes de la pagina de administraci

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdministracionRoutingModule } from './administracion-routing.module';
 import { AdministracionComponent } from './administracion/administracion/administracion.component';
 import { NavbarComponent } from './compartidos/navbar/navbar.component';
+import { NoticiasComponent } from './paginas/noticias/noticias.component';
+import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 
 @NgModule({
-  declarations: [AdministracionComponent, NavbarComponent],
+  declarations: [AdministracionComponent, NavbarComponent, NoticiasComponent, NuevohermanoComponent, ContactoComponent],
   imports: [
     CommonModule,
     AdministracionRoutingModule
