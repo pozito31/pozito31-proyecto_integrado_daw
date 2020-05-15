40e83ebafcaa2be08774ddcf58ee2ff526712bf8
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 10:37:18 +0200

    creando componentes en el modulo de pagina

---
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/componentes/contacto/contacto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto',
+  templateUrl: './contacto.component.html',
+  styleUrls: ['./contacto.component.sass']
+})
+export class ContactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/pagina.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/pagina.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaginaRoutingModule } from './pagina-routing.module';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { HermanomayorComponent } from './componentes/hermanomayor/hermanomayor.component';
+import { HistoriaComponent } from './componentes/historia/historia.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { NoticiasComponent } from './componentes/noticias/noticias.component';
+import { NovenasComponent } from './componentes/novenas/novenas.component';
+import { NuevohermanoComponent } from './componentes/nuevohermano/nuevohermano.component';
+import { ParrocoComponent } from './componentes/parroco/parroco.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { VideosComponent } from './componentes/videos/videos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CabeceraComponent, ContactoComponent, HermanomayorComponent, HistoriaComponent, ImagenesComponent, MenuComponent, NoticiasComponent, NovenasComponent, NuevohermanoComponent, ParrocoComponent, PiePaginaComponent, TableroComponent, VideosComponent],
   imports: [
     CommonModule,
     PaginaRoutingModule
