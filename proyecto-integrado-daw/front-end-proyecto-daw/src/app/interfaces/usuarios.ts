3da46b2ab65ca59a75f1bc229bfbf70a6943e519
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 14:22:13 +0200

    agregando los datos a la interfaz usuarios

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/usuarios.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/usuarios.ts
@@ -1,2 +1,13 @@
 export interface Usuarios {
+    id_usuario: number;
+    nombre: string;
+    apellidos: string;
+    fecha_alta: string;
+    usuario: string;
+    password: string;
 }
+
+export interface datosDevueltos  {
+    status: string,
+    data: Usuarios[]
+};
