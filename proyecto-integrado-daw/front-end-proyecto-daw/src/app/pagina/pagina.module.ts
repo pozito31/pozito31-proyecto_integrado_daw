37f2983d6c1e719bbfc6ae5e34e2cbc9575c6bb5
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 09:47:17 +0200

    agregando modulo pagina

---
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/pagina.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaginaRoutingModule } from './pagina-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PaginaRoutingModule
+  ]
+})
+export class PaginaModule { }
