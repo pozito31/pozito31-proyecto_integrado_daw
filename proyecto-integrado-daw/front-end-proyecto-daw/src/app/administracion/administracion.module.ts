5aa84b27b21045e0b8c83773a7d46d00dac53b93
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 10:08:03 +0200

    creando componentes de administracion

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
+import { AdministracionComponent } from './administracion/administracion/administracion.component';
+import { NoticiasComponent } from './administracion/noticias/noticias.component';
+import { NuevohermanoComponent } from './administracion/nuevohermano/nuevohermano.component';
+import { ContactoComponent } from './administracion/contacto/contacto.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdministracionComponent, NoticiasComponent, NuevohermanoComponent, ContactoComponent],
   imports: [
     CommonModule,
     AdministracionRoutingModule
