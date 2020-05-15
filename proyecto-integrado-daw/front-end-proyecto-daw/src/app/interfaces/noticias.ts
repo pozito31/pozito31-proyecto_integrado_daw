aa5d883e5a0f90cd478cbcdab0ddf3153cca7ad7
pozito31 <jessicapozo198715@gmail.com>
2020-05-15 14:36:09 +0200

    agregando datos a la interfaz noticias

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/noticias.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/interfaces/noticias.ts
@@ -1,2 +1,12 @@
 export interface Noticias {
+    id_noticia: number;
+    descripcion: string;
+    estado: string;
+    directorio_noticia: string;
+    usuarios_id_usuario: number;
+}
+
+export interface datosDevueltos  {
+    status: string;
+    data: Noticias[];
 }
