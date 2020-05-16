321f4047bd3c1b52e6fee64edfc89d9f643244d2
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 11:11:44 +0200

    agregando datos a administracion

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdministracionComponent } from './administracion/administracion/administracion.component';
+import { AdministracionComponent} from './administracion/administracion/administracion.component';
 import { NoticiasComponent } from './paginas/noticias/noticias.component';
 import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 
 const ADMIN_ROUTES: Routes = [
-  {path: '', redirectTo: 'administracion', pathMatch: 'full' },
-  {path: 'administracion', component: AdministracionComponent}, 
-  {path: 'noticias', component: NoticiasComponent}, 
-  {path: 'nuevo-hermano', component: NuevohermanoComponent}, 
-  {path: 'contacto', component: ContactoComponent}
+  { path: '', redirectTo: 'administracion', pathMatch: 'full' },
+  {
+    path: 'administracion',
+    component: AdministracionComponent
+  }, 
+  {
+    path: 'noticias',
+    component: NoticiasComponent
+  }, 
+  {
+    path: 'nuevo-hermano',
+    component: NuevohermanoComponent
+  }, 
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  }
 ];
 
 @NgModule({
