89fef48e62b47943be5a381d20ea61c361d392ab
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 09:38:38 +0200

    agregando rutas a auth

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent },
+  {path: 'registro', component: RegistroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
