5957ceb111945171cc6a88409c605f18239bfb5b
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 13:04:53 +0200

    agregando datos a login y auth

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
@@ -19,6 +21,8 @@ import { AppRoutingModule } from '../app-routing.module';
   ],
   imports: [
     CommonModule,
+    FormsModule, 
+    ReactiveFormsModule,
     AppRoutingModule,
     AuthRoutingModule
   ]
