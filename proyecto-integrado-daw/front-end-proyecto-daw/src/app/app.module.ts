a6f3a7949c9bddb53497cdbe3deca44c5ef246bb
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 11:28:52 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/app.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdministracionModule } from './administracion/administracion.module';
 import { AuthModule } from './auth/auth.module';
 import { PaginaModule } from './pagina/pagina.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,13 @@ import { PaginaModule } from './pagina/pagina.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
     AppRoutingModule,
     AdministracionModule,
     AuthModule,
-    PaginaModule
+    PaginaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
