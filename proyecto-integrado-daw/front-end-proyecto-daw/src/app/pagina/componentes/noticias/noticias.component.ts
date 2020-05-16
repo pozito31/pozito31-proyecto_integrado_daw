5378e9acf9721a078ea044f17fbac93d9e222802
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 12:33:54 +0200

    como estaba noticias

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/componentes/noticias/noticias.component.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/componentes/noticias/noticias.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-noticias',
@@ -10,36 +10,8 @@ export class NoticiasComponent implements OnInit {
 
   constructor() { }
 
-  NoticiasFormulario: FormGroup;
-
   ngOnInit(): void {
-    this.NoticiasFormulario = new FormGroup({
-      'titulo': new FormControl('', [
-         Validators.required,
-         Validators.minLength(4)
-      ]),
-      'descripcion': new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)
-      ]),
-      'texto': new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)
-      ]),
-      'imagen': new FormControl('', [
-        Validators.required
-      ])
-    });
-  }
-
-  get titulo() { return this.NoticiasFormulario.get('titulo') }
-  get descripcion() { return this.NoticiasFormulario.get('descripcion') }
-  get texto() { return this.NoticiasFormulario.get('texto') }
-  get imagen() { return this.NoticiasFormulario.get('imagen') }
-
-  onSubmit() {
-    const valor = this.NoticiasFormulario;
-    console.log(valor);
+    
   }
 
 }
