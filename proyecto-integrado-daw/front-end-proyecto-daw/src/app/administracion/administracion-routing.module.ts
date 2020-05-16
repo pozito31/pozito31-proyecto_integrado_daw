f313830992e2c3660be7ffd3f687a514c4c19a1f
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 14:11:54 +0200

    editando

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdministracionComponent} from './administracion/administracion/administracion.component';
-import { NoticiasComponent } from './paginas/noticias/noticias.component';
-import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
-import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 
 const ADMIN_ROUTES: Routes = [
@@ -12,18 +9,6 @@ const ADMIN_ROUTES: Routes = [
     path: 'administracion',
     component: AdministracionComponent
   }, 
-  {
-    path: 'noticias',
-    component: NoticiasComponent
-  }, 
-  {
-    path: 'nuevo-hermano',
-    component: NuevohermanoComponent
-  }, 
-  {
-    path: 'contacto',
-    component: ContactoComponent
-  }
 ];
 
 @NgModule({
