680523b5ce8c6ae64653e9058ec6b3ab8e62dbc4
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 11:42:48 +0200

    editando contacto

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/componentes/contacto/contacto.component.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/pagina/componentes/contacto/contacto.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -10,32 +9,8 @@ export class ContactoComponent implements OnInit {
 
   constructor() { }
 
-  ContactoFormulario: FormGroup;
-
   ngOnInit(): void {
-    this.ContactoFormulario = new FormGroup({
-      'email': new FormControl('', [
-         Validators.required,
-         Validators.minLength(4) 
-      ]),
-      'nombre': new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)
-      ]),
-      'mensaje': new FormControl('', [
-        Validators.required,
-        Validators.minLength(4)
-      ])
-    });
-  }
-
-  get email() { return this.ContactoFormulario.get('email') }
-  get nombre() { return this.ContactoFormulario.get('nombre') }
-  get mensaje() { return this.ContactoFormulario.get('mensaje') }
-
-  onSubmit() {
-    const valor = this.ContactoFormulario;
-    console.log(valor);
+   
   }
 
 }
