56f898ce8e1726e94c6ec177b5e590ecf81c1159
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 10:08:27 +0200

    agregando datos a administracion module

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
@@ -7,12 +7,14 @@ import { NavbarComponent } from './compartidos/navbar/navbar.component';
 import { NoticiasComponent } from './paginas/noticias/noticias.component';
 import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
   declarations: [AdministracionComponent, NavbarComponent, NoticiasComponent, NuevohermanoComponent, ContactoComponent],
   imports: [
     CommonModule,
+    AppRoutingModule,
     AdministracionRoutingModule
   ]
 })
