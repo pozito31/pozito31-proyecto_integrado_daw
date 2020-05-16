16608d09b6ef9388331b20d2ea49da78d275d33f
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 09:59:09 +0200

    agregando rutas a daministracion routing

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministracionComponent } from './administracion/administracion/administracion.component';
+import { NoticiasComponent } from './paginas/noticias/noticias.component';
+import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'administracion', pathMatch: 'full' },
+  {path: 'administracion', component: AdministracionComponent}, 
+  {path: 'noticias', component: NoticiasComponent}, 
+  {path: 'nuevo-hermano', component: NuevohermanoComponent}, 
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
