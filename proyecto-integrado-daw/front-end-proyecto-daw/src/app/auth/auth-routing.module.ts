db0f21a8fc87d3972c2a9d738489910d87d28d89
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 10:19:33 +0200

    editando auth-routing

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/auth/auth-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component'
 
-export const AUTH_ROUTES: Routes = [
+const AUTH_ROUTES: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent },
   {path: 'registro', component: RegistroComponent }
