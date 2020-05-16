5281c109f1b35039ba997993447011e4453da5f6
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 13:11:25 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/app.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdministracionModule } from './administracion/administracion.module';
@@ -16,8 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule, 
-    ReactiveFormsModule,
     AppRoutingModule,
     AdministracionModule,
     AuthModule,
