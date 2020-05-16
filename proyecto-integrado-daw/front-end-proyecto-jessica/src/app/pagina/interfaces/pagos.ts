e9651f56a3c66f6168788d2497653f46a496b34d
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 15:02:53 +0200

    eliminando proyecto integrado daw jessica

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/interfaces/pagos.ts
+++
@@ -1,7 +0,0 @@
-export interface Pagos {
-    id_pago: number;
-    cantidad: number;
-    realizar_pago: number;
-    fecha_transaccion: Date;
-    usuarios_id_usuarios: number
-}
--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/interfaces/usuarios.ts
+++
@@ -1,8 +0,0 @@
-export interface Usuarios {
-    id_usuarios: number;
-    nombre: string;
-    apellidos: string;
-    fecha_alta: Date;
-    usuario: string;
-    password: string
-}
