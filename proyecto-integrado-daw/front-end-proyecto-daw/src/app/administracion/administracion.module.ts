e4248a97bf32b1516c95caeb94529d7040282421
pozito31 <jessicapozo198715@gmail.com>
2020-05-16 11:07:48 +0200

    editando cosas

--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/administracion/administracion.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
-import { AdministracionComponent } from './administracion/administracion/administracion.component';
 import { NavbarComponent } from './compartidos/navbar/navbar.component';
 import { NoticiasComponent } from './paginas/noticias/noticias.component';
 import { NuevohermanoComponent } from './paginas/nuevohermano/nuevohermano.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { AdministracionComponent } from './administracion/administracion/administracion.component';
 import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
-  declarations: [AdministracionComponent, NavbarComponent, NoticiasComponent, NuevohermanoComponent, ContactoComponent],
+  declarations: [
+    AdministracionComponent,
+    NavbarComponent,
+    NoticiasComponent,
+    NuevohermanoComponent,
+    ContactoComponent
+  ],
   exports: [
     AdministracionRoutingModule
   ],
--- proyecto-integrado-daw/front-end-proyecto-daw/src/app/app-routing.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-daw/src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  
+
 ];
 
 @NgModule({
