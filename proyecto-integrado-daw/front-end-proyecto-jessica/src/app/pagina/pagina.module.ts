80a59988fddfcd8b037343e53284a41f455df4c3
pozito31 <jessicapozo198715@gmail.com>
2020-05-19 16:45:25 +0200

    agregando datos a la carpeta assets y creando cabe

--- proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/pagina.module.ts
+++ proyecto-integrado-daw/front-end-proyecto-jessica/src/app/pagina/pagina.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaginaRoutingModule } from './pagina-routing.module';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CabeceraComponent],
   imports: [
     CommonModule,
     PaginaRoutingModule
