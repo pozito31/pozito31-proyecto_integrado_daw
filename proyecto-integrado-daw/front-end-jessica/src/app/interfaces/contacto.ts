60f1364f94466e65b0a8349a56ec7c35adc5f2d2
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 17:48:40 +0200

    agregando datos a contacto.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/interfaces/contacto.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/interfaces/contacto.ts
@@ -1,2 +1,11 @@
 export interface Contacto {
+    id_contacto: number;
+    nombre: string;
+    email: string;
+    mensaje: string; 
+}
+
+export interface datosDevueltos  {
+    status: string;
+    data: Contacto[];
 }
