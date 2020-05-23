3e865b6fa392f5cbd2d4df4cc436523b2741bcb9
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 17:42:23 +0200

    agregando datos a nuevohermano.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/interfaces/nuevohermano.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/interfaces/nuevohermano.ts
@@ -1,2 +1,14 @@
 export interface Nuevohermano {
+    id_nuevohermano: number;
+    nombre: string;
+    primerapellido: string;
+    segundoapellido: string;
+    dni: string;
+    correoelectronico: string;
+    telefono: string;
+}
+
+export interface datosDevueltos  {
+    status: string;
+    data: Nuevohermano[];
 }
