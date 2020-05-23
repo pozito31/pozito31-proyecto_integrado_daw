53262fbd7313edcaf61c261647fcef2e055bfe64
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 16:52:37 +0200

    editando hermanomayor.component.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/hermanomayor/hermanomayor.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/hermanomayor/hermanomayor.component.ts
@@ -7,38 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HermanomayorComponent implements OnInit {
 
-  loadAPI: Promise<any>;
-  
+    
   constructor() {
-    this.loadAPI = new Promise((resolve) => {
-      this.loadScript();
-      resolve(true);
-    });
+    
    }
-
-   public loadScript() {        
-    var isFound = false;
-    var scripts = document.getElementsByTagName("script")
-    for (var i = 0; i < scripts.length; ++i) {
-        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
-            isFound = true;
-        }
-    }
-
-    if (!isFound) {
-        var dynamicScripts = ["assets/js/main.js"];
-
-        for (var i = 0; i < dynamicScripts.length; i++) {
-            let node = document.createElement('script');
-            node.src = dynamicScripts [i];
-            node.type = 'text/javascript';
-            node.async = false;
-            node.charset = 'utf-8';
-            document.getElementsByTagName('head')[0].appendChild(node);
-        }
-
-    }
-  }
 
   ngOnInit(): void {
   }
