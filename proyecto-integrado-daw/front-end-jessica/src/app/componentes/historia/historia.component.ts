85e1075151e494fc5de1a912867d48bf78b428f9
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 16:50:40 +0200

    editando historia.components.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/historia/historia.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/historia/historia.component.ts
@@ -6,40 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historia.component.sass']
 })
 export class HistoriaComponent implements OnInit {
-  loadAPI: Promise<any>;
-
-  constructor() {
-    this.loadAPI = new Promise((resolve) => {
-      this.loadScript();
-      resolve(true);
-    });
-   }
+  
 
   ngOnInit(): void {
   }
 
-  public loadScript() {        
-    var isFound = false;
-    var scripts = document.getElementsByTagName("script")
-    for (var i = 0; i < scripts.length; ++i) {
-        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
-            isFound = true;
-        }
-    }
-
-    if (!isFound) {
-        var dynamicScripts = ["assets/js/main.js"];
-
-        for (var i = 0; i < dynamicScripts.length; i++) {
-            let node = document.createElement('script');
-            node.src = dynamicScripts [i];
-            node.type = 'text/javascript';
-            node.async = false;
-            node.charset = 'utf-8';
-            document.getElementsByTagName('head')[0].appendChild(node);
-        }
-
-    }
-  }
+  
 
 }
