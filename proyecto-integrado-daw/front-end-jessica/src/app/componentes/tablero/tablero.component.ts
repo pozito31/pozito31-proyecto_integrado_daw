ba990a344fdecb2ae3a0b871b2b8aaaae7f1dcd0
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 16:36:50 +0200

    editando tablero

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/tablero/tablero.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/tablero/tablero.component.ts
@@ -6,39 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablero.component.sass']
 })
 export class TableroComponent implements OnInit {
-  loadAPI: Promise<any>;
-  
-  constructor() {
-    this.loadAPI = new Promise((resolve) => {
-      this.loadScript();
-      resolve(true);
-    });
-   }
 
   ngOnInit(): void {
   }
-
-  public loadScript() {        
-    var isFound = false;
-    var scripts = document.getElementsByTagName("script")
-    for (var i = 0; i < scripts.length; ++i) {
-        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
-            isFound = true;
-        }
-    }
-
-    if (!isFound) {
-        var dynamicScripts = ["assets/js/main.js"];
-
-        for (var i = 0; i < dynamicScripts.length; i++) {
-            let node = document.createElement('script');
-            node.src = dynamicScripts [i];
-            node.type = 'text/javascript';
-            node.async = false;
-            node.charset = 'utf-8';
-            document.getElementsByTagName('head')[0].appendChild(node);
-        }
-
-    }
-  }
+  
 }
