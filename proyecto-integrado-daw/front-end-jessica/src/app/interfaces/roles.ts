9af90a1eac2e6947288d2ca2c6343c5c0bae194a
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 17:37:48 +0200

    agregando datos a roles.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/interfaces/roles.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/interfaces/roles.ts
@@ -1,2 +1,13 @@
 export interface Roles {
+    id_roles: number;
+    estado: string;
+    tipo: string;
+    nombre: string;
+    situacion: string;
+    usuarios_id_usuario: number;
+}
+
+export interface datosDevueltos  {
+    status: string;
+    data: Roles[];
 }
