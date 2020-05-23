d7c7b8fae5598f37e4e5d00d8b3151d391cf5619
pozito31 <jessicapozo198715@gmail.com>
2020-05-23 17:18:20 +0200

    agregando datos a login

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/login/login.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+
+  LoginFormulario: FormGroup;
 
   ngOnInit(): void {
+    this.LoginFormulario = new FormGroup({
+      'usuario': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'password': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(15)
+      ])
+    });
+  }
+
+  get usuario() { return this.LoginFormulario.get('usuario') }
+  get password() { return this.LoginFormulario.get('password') }
+
+  onSubmit() {    
+    const val = this.LoginFormulario.value;
+    console.log(val);
   }
 
 }
