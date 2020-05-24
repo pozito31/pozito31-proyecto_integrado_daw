6e9ce1c39cb43c58af40e981f768918b6ba3f669
pozito31 <jessicapozo198715@gmail.com>
2020-05-24 14:28:15 +0200

    agregando informacion a crearnoticia

--- proyecto-integrado-daw/front-end-jessica/src/app/administracion/paginas/noticias/crearnoticia/crearnoticia.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/administracion/paginas/noticias/crearnoticia/crearnoticia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crearnoticia',
@@ -9,7 +10,40 @@ export class CrearnoticiaComponent implements OnInit {
 
   constructor() { }
 
+  NoticiasFormulario: FormGroup;
+
   ngOnInit(): void {
+    this.NoticiasFormulario = new FormGroup({
+      'titulo': new FormControl('', [
+         Validators.required,
+         Validators.minLength(4)
+      ]),
+      'descripcion': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'estado': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'texto': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'imagen': new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
+
+  get titulo() { return this.NoticiasFormulario.get('titulo') }
+  get descripcion() { return this.NoticiasFormulario.get('descripcion') }
+  get estado() { return this.NoticiasFormulario.get('estado') }
+  get texto() { return this.NoticiasFormulario.get('texto') }
+  get imagen() { return this.NoticiasFormulario.get('imagen') }
+
+  onSubmit() {
+    
   }
 
 }
