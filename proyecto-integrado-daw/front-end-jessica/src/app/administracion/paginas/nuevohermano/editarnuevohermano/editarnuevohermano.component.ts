d60b2f5992f675dc522f7f6b2ae7f7f81fb9fb73
pozito31 <jessicapozo198715@gmail.com>
2020-05-24 14:47:04 +0200

    agregando datos a editarnuevohermano

--- proyecto-integrado-daw/front-end-jessica/src/app/administracion/paginas/nuevohermano/editarnuevohermano/editarnuevohermano.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/administracion/paginas/nuevohermano/editarnuevohermano/editarnuevohermano.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editarnuevohermano',
@@ -9,7 +10,47 @@ export class EditarnuevohermanoComponent implements OnInit {
 
   constructor() { }
 
+  NuevohermanoFormulario: FormGroup;
+
   ngOnInit(): void {
+    this.NuevohermanoFormulario = new FormGroup({
+      'nombre': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'primerapellido': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'segundoapellido': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'dni': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'correoelectronico': new FormControl('', [
+         Validators.required,
+         Validators.minLength(4) 
+      ]),
+      'telefono': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    });
   }
+
+  get nombre() { return this.NuevohermanoFormulario.get('nombre') }
+  get primerapellido() { return this.NuevohermanoFormulario.get('primerapellido') }
+  get segundoapellido() { return this.NuevohermanoFormulario.get('segundoapellido') }
+  get dni() { return this.NuevohermanoFormulario.get('dni') }
+  get correoelectronico() { return this.NuevohermanoFormulario.get('correoelectronico') }
+  get telefono() { return this.NuevohermanoFormulario.get('telefono') }
+
+  onSubmit() {
+    
+  }
+
 
 }
