e1f21eac44cec37888542c21da2573380f8ee923
pozito31 <jessicapozo198715@gmail.com>
2020-05-24 18:21:26 +0200

    agregando datos a noticias.component.ts

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/noticias/noticias.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/noticias/noticias.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServicioRestNoticiasService } from '../../../../../front-end-proyecto-daw/src/app/servicios/servicio-rest-noticias.service';
+
 
 @Component({
   selector: 'app-noticias',
@@ -8,7 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class NoticiasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicioRest: ServicioRestNoticiasService) { }
 
   NoticiasFormulario: FormGroup;
 
