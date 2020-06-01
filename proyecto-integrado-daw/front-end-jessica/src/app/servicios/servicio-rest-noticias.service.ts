d0d76444968fc04242adf0447d1d2ed75c4e4217
pozito31 <jessicapozo198715@gmail.com>
2020-06-01 16:32:18 +0200

    agregando datos al servicio de noticias

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/servicio-rest-noticias.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/servicio-rest-noticias.service.ts
@@ -3,8 +3,9 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { DatosProtegidosService } from "./datos-protegidos.service";
-import { datosDevueltos } from '../interfaces/noticias';
-import { Noticias } from '../../../../front-end-proyecto-daw/src/app/interfaces/noticias';
+import { Noticias, datosDevueltos } from '../interfaces/noticias';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,13 @@ export class ServicioRestNoticiasService {
 
   constructor(private http: HttpClient, private direcciones : DatosProtegidosService) { }
   
+  agregarNoticia(noticia: datosDevueltos){
+    return this.http.post('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/noticias', noticia)
+    .pipe(
+      retry(3), //Si ocurre un error lo volvemos a intentar hasta tres veces
+      catchError(this.handleError) //Gestionar el error
+    );
+  }
   addNoticias(id_noticia:number){
     return this.http.get<datosDevueltos>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/noticias'+'/'+id_noticia);
   }
