dfe26e97e94a79e8e7357da9801516ff4138d69a
pozito31 <jessicapozo198715@gmail.com>
2020-06-04 12:00:00 +0200

    agregando datos a contacto.component

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/contacto/contacto.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/contacto/contacto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServicioRestContactoService } from '../../servicios/servicio-rest-contacto.service';
+import { Contacto } from '../../interfaces/contacto';
+
 
 @Component({
   selector: 'app-contacto',
@@ -8,7 +11,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ServicioRestContactoService: ServicioRestContactoService) { }
+  nuevoContacto: Contacto = {
+    id_contacto: 0,
+    nombre: "a",
+    email: "a",
+    mensaje: "a"
+  };
 
   ContactoFormulario: FormGroup;
 
@@ -34,7 +43,11 @@ export class ContactoComponent implements OnInit {
   get mensaje() { return this.ContactoFormulario.get('mensaje') }
 
   onSubmit() {
-   
+    this.nuevoContacto.email = this.ContactoFormulario.get("email").value;
+    this.nuevoContacto.nombre = this.ContactoFormulario.get("nombre").value;
+    this.nuevoContacto.mensaje = this.ContactoFormulario.get("mensaje").value;
+    console.log(this.ContactoFormulario);
+    this.ServicioRestContactoService.añadirContacto(this.nuevoContacto).subscribe();
   }
 
 }
