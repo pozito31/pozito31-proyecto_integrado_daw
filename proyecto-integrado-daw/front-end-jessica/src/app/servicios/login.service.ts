6a50f4f31b9c0779106160b90c5a3d00981ae044
pozito31 <jessicapozo198715@gmail.com>
2020-06-10 17:06:08 +0200

    agregando datos a servicio login

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Usuarios } from '../interfaces/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  login(usuario: string, password: string){
+    return this.http.post<any>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/usuarios', { usuario: usuario, password: password })
+    .pipe(map(usuarios => {
+      // login successful if there's a jwt token in the response
+      if (usuarios && usuarios.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(usuarios));
+      }
+
+      return usuarios;
+    }));
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
   }
 
 }
