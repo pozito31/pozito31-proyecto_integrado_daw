34ba0ba44217b9fb14248e2f7789609832c1ad9b
pozito31 <jessicapozo198715@gmail.com>
2020-06-10 17:14:31 +0200

    actualizar servicio login

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -11,22 +11,6 @@ export class LoginService {
   constructor(private http: HttpClient) {
   }
 
-  login(usuario: string, password: string){
-    return this.http.post<any>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/usuarios', { usuario: usuario, password: password })
-    .pipe(map(usuarios => {
-      // login successful if there's a jwt token in the response
-      if (usuarios && usuarios.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify(usuarios));
-      }
-
-      return usuarios;
-    }));
-  }
-
-  logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
-  }
+  
 
 }
