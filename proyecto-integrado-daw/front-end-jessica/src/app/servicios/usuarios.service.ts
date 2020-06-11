c8f98c40571c1e1d36ae96ef4ffc9fef21e4333e
pozito31 <jessicapozo198715@gmail.com>
2020-06-11 17:44:03 +0200

    agregando datos al servicio usuarios

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/usuarios.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/usuarios.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuarios } from '../interfaces/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<Usuarios[]>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/usuarios');
+  }
 }
