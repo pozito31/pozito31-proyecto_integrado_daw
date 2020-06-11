b675f5cd1e46a63050627da8a1f632f32fd88fcd
pozito31 <jessicapozo198715@gmail.com>
2020-06-11 17:35:26 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Usuarios } from '../interfaces/usuarios';
 
@@ -7,10 +8,32 @@ import { Usuarios } from '../interfaces/usuarios';
   providedIn: 'root'
 })
 export class LoginService {
+  private currentUserSubject: BehaviorSubject<Usuarios>;
+  public currentUser: Observable<Usuarios>;
 
   constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<Usuarios>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+  
+  public get currentUserValue(): Usuarios {
+    return this.currentUserSubject.value;
   }
 
-  
+  login(usuario: string, password: string) {
+    return this.http.post<any>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/usuarios', { usuario, password })
+        .pipe(map(usuarios => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(usuarios));
+            this.currentUserSubject.next(usuarios);
+            return usuarios;
+        }));
+  }
 
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+  
 }
