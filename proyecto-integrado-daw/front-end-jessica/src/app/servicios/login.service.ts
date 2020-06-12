7619d81d197c1a996dd88fd309c1b9b58428a87e
pozito31 <jessicapozo198715@gmail.com>
2020-06-12 11:55:10 +0200

    eliminando cosas y editando

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -8,32 +8,12 @@ import { Usuarios } from '../interfaces/usuarios';
   providedIn: 'root'
 })
 export class LoginService {
-  private currentUserSubject: BehaviorSubject<Usuarios>;
-  public currentUser: Observable<Usuarios>;
+  
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<Usuarios>(JSON.parse(localStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
+    
   }
   
-  public get currentUserValue(): Usuarios {
-    return this.currentUserSubject.value;
-  }
-
-  login(usuario: string, password: string) {
-    return this.http.post<any>('http://pi.diiesmurgi.org/~jessica/REST_API/api/v1/usuarios', { usuario, password })
-        .pipe(map(usuarios => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('currentUser', JSON.stringify(usuarios));
-            this.currentUserSubject.next(usuarios);
-            return usuarios;
-        }));
-  }
-
-  logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
-  }
+  
   
 }
