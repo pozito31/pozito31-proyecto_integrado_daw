35e6e82e3a6d51a91e509059acf65cb42c03f24b
pozito31 <jessicapozo198715@gmail.com>
2020-06-13 18:49:19 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/usuarios.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/usuarios.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Usuarios } from '../interfaces/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<Usuarios[]>('');
+  }
 }
--- proyecto-integrado-daw/front-end-jessica/src/environments/environment.ts
+++ proyecto-integrado-daw/front-end-jessica/src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://pi.diiesmurgi.org/~jessica/rest-api/api/v1/usuarios'
+  production: false
 };
 
 /*
