8647e48717b8e5d4470a35cc1de4594a0ce62470
pozito31 <jessicapozo198715@gmail.com>
2020-06-14 12:12:42 +0200

    editando login servicio

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -10,32 +10,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class LoginService {
-  private currentUserSubject: BehaviorSubject<Usuarios>;
-  public currentUser: Observable<Usuarios>;
   
-  constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<Usuarios>(JSON.parse(localStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
-
-  public get currentUserValue(): Usuarios {
-    return this.currentUserSubject.value;
-}
-
-  login(usuario, password) {
-    return this.http.post<any>(`${environment.apiUrl}/usuarios/authenticate`, { usuario, password })
-        .pipe(map(usuarios => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('currentUser', JSON.stringify(usuarios));
-            this.currentUserSubject.next(usuarios);
-            return usuarios;
-        }));
-  }
-
-  logout() {
-    // remove user from local storage and set current user to null
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
-  }
+  constructor(private http: HttpClient) {}
   
 }
