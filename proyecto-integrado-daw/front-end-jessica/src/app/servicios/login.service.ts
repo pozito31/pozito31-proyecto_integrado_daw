0c2cf5fcb6d6156d94acf1d7b6bd0b25ec3aaa7e
pozito31 <jessicapozo198715@gmail.com>
2020-06-14 12:13:42 +0200

    editando ficheros

--- proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/servicios/login.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Usuarios } from '../interfaces/usuarios';
-import { environment } from '../../environments/environment';
 
 
 @Injectable({
