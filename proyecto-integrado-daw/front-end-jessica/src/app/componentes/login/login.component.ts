b9e84b96c8e132ae339f5e23ea21bab5c4a159e1
pozito31 <jessicapozo198715@gmail.com>
2020-06-14 15:31:27 +0200

    agregando datos

--- proyecto-integrado-daw/front-end-jessica/src/app/componentes/login/login.component.ts
+++ proyecto-integrado-daw/front-end-jessica/src/app/componentes/login/login.component.ts
@@ -16,8 +16,6 @@ export class LoginComponent implements OnInit {
   LoginFormulario: FormGroup;
   constructor(private router: Router, private LoginService: LoginService) {}
   
- 
-
   ngOnInit(): void {
     this.LoginFormulario = new FormGroup({
       'usuario': new FormControl('', [
@@ -37,20 +35,13 @@ export class LoginComponent implements OnInit {
   get password() { return this.LoginFormulario.get('password') }
 
   onSubmit() {
-    this.LoginService.login(this.LoginFormulario.value).subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-    if(this.LoginFormulario){
-      this.router.navigate(['/administracion']);
-    }else{
-      this.router.navigate(['/']);
-    }
-    
+    this.LoginService.login(this.LoginFormulario.value).subscribe((res) => {
+      if(res){
+          this.router.navigate(['/administracion']);
+        }else{
+          this.router.navigate(['/']);
+        }
+    })
   }
 
 }
