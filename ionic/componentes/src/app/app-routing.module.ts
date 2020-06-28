b4df3a33cbcd5ffda91b9cf46800b8913b89d387
pozito31 <jessicapozo198715@gmail.com>
2020-06-28 15:16:09 +0200

    agregando cosas a la aplicacion ionic

--- ionic/componentes/src/app/app-routing.module.ts
+++ ionic/componentes/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
 ];
 
