86be05049717a3dfa87f40bebed8c8f0660d4221
pozito31 <jessicapozo198715@gmail.com>
2020-06-28 16:13:27 +0200

    agregando datos

--- ionic/componentes/src/app/pages/action-sheet/action-sheet.module.ts
+++ ionic/componentes/src/app/pages/action-sheet/action-sheet.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ActionSheetPageRoutingModule } from './action-sheet-routing.module';
 
 import { ActionSheetPage } from './action-sheet.page';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ActionSheetPageRoutingModule
+    ActionSheetPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ActionSheetPage]
 })
