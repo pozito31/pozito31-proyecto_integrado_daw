af1b20bcb17c2daf8179e8735d28c04644289320
pozito31 <jessicapozo198715@gmail.com>
2020-06-29 15:29:52 +0200

    agregando datos

--- vue/proyecto-2/src/components/Lista.vue
+++ vue/proyecto-2/src/components/Lista.vue
@@ -1,24 +1,26 @@
 <template>
     <div>
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Cras justo odio
-                <span class="badge badge-primary badge-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Dapibus ac facilisis in
-                <span class="badge badge-primary badge-pill">2</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Morbi leo risus
-                <span class="badge badge-primary badge-pill">1</span>
+            <li v-for="(item, index) of frutas" :key="item.id" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}} - {{item.nombre}}
+                <span class="badge badge-primary badge-pill"> {{item.cantidad}}</span>
             </li>
         </ul>
+        <button class="btn btn-danger btn-block" @click="reiniciar">Reiniciar</button>
     </div>
 </template>
 
 <script>
+    import {mapState, mapMutations} from 'vuex';
+
     export default {
-        name: 'Lista'
+        name: 'Lista',
+        computed:{
+            ...mapState(['frutas'])
+        },
+        methods: {
+          ...mapMutations(['aumentar', 'reiniciar'])  
+        }
     }
 </script>
--- vue/proyecto-2/src/store/index.js
+++ vue/proyecto-2/src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Naranja', cantidad: 0},
+    ]
   },
   mutations: {
   },
   actions: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++
+    },
+    reiniciar(state){
+      state.frutas.forEach(elemento => {
+        elemento.cantidad = 0
+      })
+    }
   },
   modules: {
   }
