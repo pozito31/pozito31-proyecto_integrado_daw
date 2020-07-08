70a16cdceca10c7c76931afde9f3e5e6c70ec8b4
pozito31 <jessicapozo198715@gmail.com>
2020-07-08 15:23:18 +0200

    agregando el componente portafolio

--- proyecto-integrado-daw/portafolio/src/app/app.module.ts
+++ proyecto-integrado-daw/portafolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
