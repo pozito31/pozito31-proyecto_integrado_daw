c7e7faca5c96b1a461667895a478bf4c2b21af38
pozito31 <jessicapozo198715@gmail.com>
2020-07-12 15:16:30 +0200

    creando el componente about

--- proyecto-integrado-daw/portafolio/src/app/app.module.ts
+++ proyecto-integrado-daw/portafolio/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
