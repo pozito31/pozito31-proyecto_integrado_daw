a85f9852f4a531d4bd3436c92370acbbae91e2d1
pozito31 <jessicapozo198715@gmail.com>
2020-08-12 12:21:00 +0200

    cregando en la aplicacion zapateria los componente

--- zapateria/src/app/app.module.ts
+++ zapateria/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
