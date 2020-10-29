106487b5448fedcd0d82137612a728cad964f665
pozito31 <jessicapozo198715@gmail.com>
2020-10-29 11:09:57 +0100

    subir Codigo de XAML de ImageButton

---
+++ ImageButton/ImageButton/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageButton
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Prueba", "Has presionado un control de ImageButton", "OK");
+        }
+    }
+}
